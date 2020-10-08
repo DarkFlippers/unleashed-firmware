3067b32a52487afa8030bcac413415051a9239e4
DrZlo13 <who.just.the.doctor@gmail.com>
2020-10-09 02:34:43 +1000

    Fix input (#166)
    * fixed inline functions for modern C standart
    
    * Fix button-left and button-back setup code
    
    * *nix fix
    
    Co-authored-by: aanper <mail@s3f.ru>

--- firmware/targets/f2/Inc/main.h
+++ firmware/targets/f2/Inc/main.h
@@ -60,6 +60,7 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define BUTTON_BACK_Pin GPIO_PIN_13
 #define BUTTON_BACK_GPIO_Port GPIOC
+#define BUTTON_BACK_EXTI_IRQn EXTI15_10_IRQn
 #define CHRG_Pin GPIO_PIN_2
 #define CHRG_GPIO_Port GPIOC
 #define CHRG_EXTI_IRQn EXTI2_IRQn
