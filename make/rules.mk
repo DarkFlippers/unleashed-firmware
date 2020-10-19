0829a04d2022e2e5fe2b79f6e446218d4ba5a345
aanper <mail@s3f.ru>
2020-10-19 19:31:24 +0300

    use arm-none-eabi-gdb from ubuntu-docker

--- make/rules.mk
+++ make/rules.mk
@@ -68,7 +68,7 @@ upload: $(OBJ_DIR)/upload
 
 debug: flash
 	set -m; st-util -n --semihosting & echo $$! > $(OBJ_DIR)/st-util.PID
-	arm-none-eabi-gdb-py \
+	arm-none-eabi-gdb \
 		-ex "target extended-remote 127.0.0.1:4242" \
 		-ex "set confirm off" \
 		-ex "source ../debug/FreeRTOS/FreeRTOS.py" \
