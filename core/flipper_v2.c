2ba3722de202de49150a02155689bbbc6964f70e
DrZlo13 <who.just.the.doctor@gmail.com>
2020-11-16 20:22:28 +0300

    add mutex in furi_create_deprecated (#242)

--- core/flipper_v2.c
+++ core/flipper_v2.c
@@ -1,5 +1,15 @@
 #include "flipper_v2.h"
 
 bool init_flipper_api(void) {
-    return gpio_api_init();
+    bool no_errors = true;
+
+    if(!furi_init()) {
+        no_errors = false;
+    }
+
+    if(!gpio_api_init()) {
+        no_errors = false;
+    }
+
+    return no_errors;
 }
