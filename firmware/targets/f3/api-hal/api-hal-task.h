fb76b6c96caef653608e1827456db8bb33a94201
aanper <mail@s3f.ru>
2020-12-10 17:58:33 +0300

    increase max app count

--- firmware/targets/f3/api-hal/api-hal-task.h
+++ firmware/targets/f3/api-hal/api-hal-task.h
@@ -7,7 +7,7 @@
 #define DEFAULT_STACK_SIZE 4096
 
 // Max system tasks count
-#define MAX_TASK_COUNT 10
+#define MAX_TASK_COUNT 14
 
 bool task_equal(TaskHandle_t a, TaskHandle_t b);
 bool task_is_isr_context(void);
