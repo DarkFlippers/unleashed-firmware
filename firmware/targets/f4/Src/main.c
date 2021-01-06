f0ea3c4310fb275ff0c2997734e47dcef936ee32
あく <alleteam@gmail.com>
2021-01-06 23:03:37 +0300

    Power: set smps clock according to used inductor. RTC: do not reset clock on each start. (#281)

--- firmware/targets/f4/Src/main.c
+++ firmware/targets/f4/Src/main.c
@@ -215,7 +215,7 @@ void SystemClock_Config(void)
   PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
   PeriphClkInitStruct.RFWakeUpClockSelection = RCC_RFWKPCLKSOURCE_LSE;
   PeriphClkInitStruct.SmpsClockSelection = RCC_SMPSCLKSOURCE_HSE;
-  PeriphClkInitStruct.SmpsDivSelection = RCC_SMPSCLKDIV_RANGE0;
+  PeriphClkInitStruct.SmpsDivSelection = RCC_SMPSCLKDIV_RANGE1;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
   {
     Error_Handler();
