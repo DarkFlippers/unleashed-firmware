a3b7940baf343c5485ff8c2c7baa7bb90cb5473a
SG <who.just.the.doctor@gmail.com>
2021-03-09 03:00:56 +1000

    Udpate gui_test and app_subghz stack size (#368)

--- applications/applications.c
+++ applications/applications.c
@@ -147,11 +147,11 @@ const FlipperApplication FLIPPER_SERVICES[] = {
 #endif
 
 #ifdef APP_GUI_TEST
-    {.app = gui_test, .name = "gui_test", .icon = A_Plugins_14},
+    {.app = gui_test, .name = "gui_test", .stack_size = 1024, .icon = A_Plugins_14},
 #endif
 
 #ifdef APP_SUBGHZ
-    {.app = app_subghz, .name = "app_subghz", .icon = A_Plugins_14},
+    {.app = app_subghz, .name = "app_subghz", .stack_size = 1024, .icon = A_Plugins_14},
 #endif
 };
 
@@ -223,11 +223,11 @@ const FlipperApplication FLIPPER_PLUGINS[] = {
 #endif
 
 #ifdef BUILD_GUI_TEST
-    {.app = gui_test, .name = "gui_test", .icon = A_Plugins_14},
+    {.app = gui_test, .name = "gui_test", .stack_size = 1024, .icon = A_Plugins_14},
 #endif
 
 #ifdef BUILD_SUBGHZ
-    {.app = app_subghz, .name = "app_subghz", .icon = A_Plugins_14},
+    {.app = app_subghz, .name = "app_subghz", .stack_size = 1024, .icon = A_Plugins_14},
 #endif
 };
 
