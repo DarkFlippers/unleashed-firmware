7d3b55272d230d1da1fe6f0357791ec0c0978039
あく <alleteam@gmail.com>
2021-04-30 14:07:13 +0300

    Recator application build system: cleaner flags naming, set release flag to yes by default, include unit test and debug apps (#443)

--- applications/applications.c
+++ applications/applications.c
@@ -1,9 +1,6 @@
 #include "applications.h"
 
-#ifdef APP_TEST
 int32_t flipper_test_app(void* p);
-#endif
-
 int32_t application_blink(void* p);
 int32_t application_uart_write(void* p);
 int32_t application_input_dump(void* p);
@@ -38,23 +35,23 @@ int32_t passport(void* p);
 int32_t app_accessor(void* p);
 
 const FlipperApplication FLIPPER_SERVICES[] = {
-#ifdef APP_CLI
+#ifdef SRV_CLI
     {.app = cli_task, .name = "cli_task", .stack_size = 2048, .icon = A_Plugins_14},
 #endif
 
-#ifdef APP_EXAMPLE_BLINK
+#ifdef SRV_EXAMPLE_BLINK
     {.app = application_blink, .name = "blink", .stack_size = 1024, .icon = A_Plugins_14},
 #endif
 
-#ifdef APP_INPUT
+#ifdef SRV_INPUT
     {.app = input_task, .name = "input_task", .stack_size = 1024, .icon = A_Plugins_14},
 #endif
 
-#ifdef APP_EXAMPLE_INPUT_DUMP
+#ifdef SRV_EXAMPLE_INPUT_DUMP
     {.app = application_input_dump, .name = "input dump", .stack_size = 1024, .icon = A_Plugins_14},
 #endif
 
-#ifdef APP_GUI
+#ifdef SRV_GUI
     {.app = backlight_control,
      .name = "backlight_control",
      .stack_size = 1024,
@@ -63,52 +60,131 @@ const FlipperApplication FLIPPER_SERVICES[] = {
     {.app = gui_task, .name = "gui_task", .stack_size = 8192, .icon = A_Plugins_14},
 #endif
 
-#ifdef APP_MENU
+#ifdef SRV_MENU
     {.app = menu_task, .name = "menu_task", .stack_size = 1024, .icon = A_Plugins_14},
     {.app = app_loader, .name = "app_loader", .stack_size = 1024, .icon = A_Plugins_14},
 #endif
 
-#ifdef APP_SD_FILESYSTEM
+#ifdef SRV_SD_FILESYSTEM
     {.app = sd_filesystem, .name = "sd_filesystem", .stack_size = 4096, .icon = A_Plugins_14},
 #endif
 
-#ifdef APP_DOLPHIN
+#ifdef SRV_DOLPHIN
     {.app = dolphin_task, .name = "dolphin_task", .stack_size = 1024, .icon = A_Plugins_14},
 #endif
 
-#ifdef APP_POWER
+#ifdef SRV_POWER
     {.app = power_task, .name = "power_task", .stack_size = 1024, .icon = A_Plugins_14},
 #endif
 
-#ifdef APP_BT
+#ifdef SRV_BT
     {.app = bt_task, .name = "bt_task", .stack_size = 1024, .icon = A_Plugins_14},
 #endif
 
-#ifdef APP_LF_RFID
+#ifdef SRV_LF_RFID
     {.app = lf_rfid_workaround,
      .name = "lf rfid workaround",
      .stack_size = 1024,
      .icon = A_Plugins_14},
 #endif
 
-#ifdef APP_IRDA
+#ifdef SRV_IRDA
     {.app = irda, .name = "irda", .stack_size = 1024, .icon = A_Plugins_14},
 #endif
 
-#ifdef APP_NFC
+#ifdef SRV_NFC
     {.app = nfc_task, .name = "nfc_task", .stack_size = 1024, .icon = A_Plugins_14},
 #endif
 
-#ifdef APP_TEST
-    {.app = flipper_test_app, .name = "test app", .stack_size = 1024, .icon = A_Plugins_14},
-#endif
-
-#ifdef APP_EXAMPLE_QRCODE
+#ifdef SRV_EXAMPLE_QRCODE
     {.app = u8g2_qrcode, .name = "u8g2_qrcode", .stack_size = 1024, .icon = A_Plugins_14},
 #endif
 
-#ifdef APP_EXAMPLE_DISPLAY
+#ifdef SRV_EXAMPLE_DISPLAY
     {.app = u8g2_example, .name = "u8g2_example", .stack_size = 1024, .icon = A_Plugins_14},
+#endif
+
+#ifdef SRV_SPEAKER_DEMO
+    {.app = coreglitch_demo_0,
+     .name = "coreglitch_demo_0",
+     .stack_size = 1024,
+     .icon = A_Plugins_14},
+#endif
+
+#ifdef SRV_SD_TEST
+    {.app = sd_card_test, .name = "sd_card_test", .stack_size = 4096, .icon = A_Plugins_14},
+#endif
+
+#ifdef SRV_MUSIC_PLAYER
+    {.app = music_player, .name = "music player", .stack_size = 1024, .icon = A_Plugins_14},
+#endif
+
+#ifdef SRV_IBUTTON
+    {.app = app_ibutton, .name = "ibutton", .stack_size = 4096, .icon = A_Plugins_14},
+#endif
+
+#ifdef SRV_GPIO_DEMO
+    {.app = app_gpio_test, .name = "gpio test", .stack_size = 1024, .icon = A_Plugins_14},
+#endif
+
+#ifdef SRV_FLOOPPER_BLOOPPER
+    {.app = floopper_bloopper, .name = "Floopper Bloopper", .stack_size = 1024, .icon = A_Games_14},
+#endif
+
+#ifdef SRV_SDNFC
+    {.app = sdnfc, .name = "sdnfc", .stack_size = 1024, .icon = A_Plugins_14},
+#endif
+
+#ifdef SRV_GUI_TEST
+    {.app = gui_test, .name = "gui_test", .stack_size = 1024, .icon = A_Plugins_14},
+#endif
+
+#ifdef SRV_KEYPAD_TEST
+    {.app = keypad_test, .name = "keypad_test", .icon = A_Plugins_14},
+#endif
+
+#ifdef SRV_ACCESSOR
+    {.app = app_accessor, .name = "accessor", .stack_size = 4096, .icon = A_Plugins_14},
+#endif
+
+};
+
+const size_t FLIPPER_SERVICES_COUNT = sizeof(FLIPPER_SERVICES) / sizeof(FlipperApplication);
+
+// Main menu APP
+const FlipperApplication FLIPPER_APPS[] = {
+#ifdef APP_SUBGHZ
+    {.app = subghz_app, .name = "Sub-1 GHz", .stack_size = 1024, .icon = A_Sub1ghz_14},
+#endif
+
+#ifdef APP_LF_RFID
+    {.app = lf_rfid_workaround, .name = "125 kHz RFID", .stack_size = 1024, .icon = A_125khz_14},
+#endif
+
+#ifdef APP_IRDA
+    {.app = irda, .name = "Infrared", .stack_size = 1024, .icon = A_Infrared_14},
+#endif
+
+#ifdef APP_IBUTTON
+    {.app = app_ibutton, .name = "iButton", .stack_size = 4096, .icon = A_iButton_14},
+#endif
+
+#ifdef APP_GPIO_DEMO
+    {.app = app_gpio_test, .name = "GPIO", .stack_size = 1024, .icon = A_GPIO_14},
+#endif
+};
+
+const size_t FLIPPER_APPS_COUNT = sizeof(FLIPPER_APPS) / sizeof(FlipperApplication);
+
+// Plugin menu
+const FlipperApplication FLIPPER_PLUGINS[] = {
+
+#ifdef APP_MUSIC_PLAYER
+    {.app = music_player, .name = "music player", .stack_size = 1024, .icon = A_Plugins_14},
+#endif
+
+#ifdef APP_FLOOPPER_BLOOPPER
+    {.app = floopper_bloopper, .name = "Floopper Bloopper", .stack_size = 1024, .icon = A_Games_14},
 #endif
 
 #ifdef APP_SPEAKER_DEMO
@@ -118,24 +194,26 @@ const FlipperApplication FLIPPER_SERVICES[] = {
      .icon = A_Plugins_14},
 #endif
 
+};
+
+const size_t FLIPPER_PLUGINS_COUNT = sizeof(FLIPPER_PLUGINS) / sizeof(FlipperApplication);
+
+// Plugin menu
+const FlipperApplication FLIPPER_DEBUG_APPS[] = {
+#ifdef APP_EXAMPLE_BLINK
+    {.app = application_blink, .name = "blink", .stack_size = 1024, .icon = A_Plugins_14},
+#endif
+
+#ifdef APP_EXAMPLE_INPUT_DUMP
+    {.app = application_input_dump, .name = "input dump", .stack_size = 1024, .icon = A_Plugins_14},
+#endif
+
 #ifdef APP_SD_TEST
     {.app = sd_card_test, .name = "sd_card_test", .stack_size = 4096, .icon = A_Plugins_14},
 #endif
 
-#ifdef APP_MUSIC_PLAYER
-    {.app = music_player, .name = "music player", .stack_size = 1024, .icon = A_Plugins_14},
-#endif
-
-#ifdef APP_IBUTTON
-    {.app = app_ibutton, .name = "ibutton", .stack_size = 4096, .icon = A_Plugins_14},
-#endif
-
-#ifdef APP_GPIO_DEMO
-    {.app = app_gpio_test, .name = "gpio test", .stack_size = 1024, .icon = A_Plugins_14},
-#endif
-
-#ifdef APP_FLOOPPER_BLOOPPER
-    {.app = floopper_bloopper, .name = "Floopper Bloopper", .stack_size = 1024, .icon = A_Games_14},
+#ifdef APP_VIBRO_DEMO
+    {.app = application_vibro, .name = "vibro", .stack_size = 1024, .icon = A_Plugins_14},
 #endif
 
 #ifdef APP_SDNFC
@@ -154,96 +232,15 @@ const FlipperApplication FLIPPER_SERVICES[] = {
     {.app = app_accessor, .name = "accessor", .stack_size = 4096, .icon = A_Plugins_14},
 #endif
 
-};
-
-const size_t FLIPPER_SERVICES_COUNT = sizeof(FLIPPER_SERVICES) / sizeof(FlipperApplication);
-
-// Main menu APP
-const FlipperApplication FLIPPER_APPS[] = {
-#ifdef BUILD_SUBGHZ
-    {.app = subghz_app, .name = "Sub-1 GHz", .stack_size = 1024, .icon = A_Sub1ghz_14},
-#endif
-
-#ifdef BUILD_LF_RFID
-    {.app = lf_rfid_workaround, .name = "125 kHz RFID", .stack_size = 1024, .icon = A_125khz_14},
-#endif
-
-#ifdef BUILD_IRDA
-    {.app = irda, .name = "Infrared", .stack_size = 1024, .icon = A_Infrared_14},
-#endif
-
-#ifdef BUILD_IBUTTON
-    {.app = app_ibutton, .name = "iButton", .stack_size = 4096, .icon = A_iButton_14},
-#endif
-
-#ifdef BUILD_GPIO_DEMO
-    {.app = app_gpio_test, .name = "GPIO", .stack_size = 1024, .icon = A_GPIO_14},
-#endif
-};
-
-const size_t FLIPPER_APPS_COUNT = sizeof(FLIPPER_APPS) / sizeof(FlipperApplication);
-
-// Plugin menu
-const FlipperApplication FLIPPER_PLUGINS[] = {
-
-#ifdef BUILD_MUSIC_PLAYER
-    {.app = music_player, .name = "music player", .stack_size = 1024, .icon = A_Plugins_14},
-#endif
-
-#ifdef BUILD_FLOOPPER_BLOOPPER
-    {.app = floopper_bloopper, .name = "Floopper Bloopper", .stack_size = 1024, .icon = A_Games_14},
-#endif
-
-#ifdef BUILD_SPEAKER_DEMO
-    {.app = coreglitch_demo_0,
-     .name = "coreglitch_demo_0",
-     .stack_size = 1024,
-     .icon = A_Plugins_14},
-#endif
-
-};
-
-const size_t FLIPPER_PLUGINS_COUNT = sizeof(FLIPPER_PLUGINS) / sizeof(FlipperApplication);
-
-// Plugin menu
-const FlipperApplication FLIPPER_DEBUG_APPS[] = {
-#ifdef BUILD_EXAMPLE_BLINK
-    {.app = application_blink, .name = "blink", .stack_size = 1024, .icon = A_Plugins_14},
-#endif
-
-#ifdef BUILD_EXAMPLE_INPUT_DUMP
-    {.app = application_input_dump, .name = "input dump", .stack_size = 1024, .icon = A_Plugins_14},
-#endif
-
-#ifdef BUILD_SD_TEST
-    {.app = sd_card_test, .name = "sd_card_test", .stack_size = 4096, .icon = A_Plugins_14},
-#endif
-
-#ifdef BUILD_VIBRO_DEMO
-    {.app = application_vibro, .name = "vibro", .stack_size = 1024, .icon = A_Plugins_14},
-#endif
-
-#ifdef BUILD_SDNFC
-    {.app = sdnfc, .name = "sdnfc", .stack_size = 1024, .icon = A_Plugins_14},
-#endif
-
-#ifdef BUILD_GUI_TEST
-    {.app = gui_test, .name = "gui_test", .stack_size = 1024, .icon = A_Plugins_14},
-#endif
-
-#ifdef BUILD_KEYPAD_TEST
-    {.app = keypad_test, .name = "keypad_test", .icon = A_Plugins_14},
-#endif
-
-#ifdef BUILD_ACCESSOR
-    {.app = app_accessor, .name = "accessor", .stack_size = 4096, .icon = A_Plugins_14},
+#ifdef APP_UNIT_TESTS
+    {.app = flipper_test_app, .name = "Unit Tests", .stack_size = 1024, .icon = A_Plugins_14},
 #endif
 
 };
 
 const size_t FLIPPER_DEBUG_APPS_COUNT = sizeof(FLIPPER_DEBUG_APPS) / sizeof(FlipperApplication);
 
-#ifdef APP_DOLPHIN
+#ifdef SRV_DOLPHIN
 const FlipperApplication FLIPPER_SCENE =
     {.app = scene_app, .name = "Scenes", .stack_size = 1024, .icon = A_Games_14};
 
