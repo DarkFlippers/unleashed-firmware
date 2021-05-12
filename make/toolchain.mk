0f186e10fa3bbaf444aa93ba5131c6099b4baaef
あく <alleteam@gmail.com>
2021-05-12 10:59:20 +0300

    [FL-1241] Build optimization (#459)
    * Makefile: enable space optimization with debug, cleanup double includes, explicitly define cpp runtime options
    * Toolchain: add printf support
    * Toolchain: explicit standard declaration

--- make/toolchain.mk
+++ make/toolchain.mk
@@ -6,8 +6,8 @@ PREFIX = $(GCC_PATH)/$(PREFIX)
 endif
 endif
 
-CC	= $(PREFIX)gcc
-CPP	= $(PREFIX)g++
+CC	= $(PREFIX)gcc -std=gnu17
+CPP	= $(PREFIX)g++ -std=gnu++17
 LD	= $(PREFIX)g++
 AS	= $(PREFIX)gcc -x assembler-with-cpp
 CP	= $(PREFIX)objcopy
@@ -16,12 +16,15 @@ HEX	= $(CP) -O ihex
 BIN	= $(CP) -O binary -S
 
 DEBUG ?= 1
+COMPACT ?= 0
 ifeq ($(DEBUG), 1)
-CFLAGS += -DDEBUG -g
+CFLAGS += -DDEBUG -Og -g
+else ifeq ($(COMPACT), 1)
+CFLAGS += -DNDEBUG -DLFS_NO_ASSERT -Os
 else
-CFLAGS += -DNDEBUG -Os
+CFLAGS += -DNDEBUG -DLFS_NO_ASSERT -Og
 endif
 
-CFLAGS		+= -MMD -MP -MF"$(@:%.o=%.d)"
-CPPFLAGS	+= -fno-threadsafe-statics
-LDFLAGS		+= -Wl,-Map=$(OBJ_DIR)/$(PROJECT).map,--cref -Wl,--gc-sections -Wl,--undefined=uxTopUsedPriority
+CFLAGS		+= -fdata-sections -ffunction-sections -fno-math-errno -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)"
+CPPFLAGS	+= -fno-threadsafe-statics -fno-use-cxa-atexit -fno-exceptions -fno-rtti
+LDFLAGS		+= -Wl,-Map=$(OBJ_DIR)/$(PROJECT).map,--cref -Wl,--gc-sections -Wl,--undefined=uxTopUsedPriority -u _printf_float
