588480831aa69d0450393d7fc7cfd6b33516e45c
gornekich <n.gorbadey@gmail.com>
2021-05-13 16:47:55 +0300

    [FL-1214] nfc_worker fix emulation

--- applications/nfc/nfc_worker.c
+++ applications/nfc/nfc_worker.c
@@ -185,6 +185,7 @@ void nfc_worker_emulate(NfcWorker* nfc_worker) {
     params.lmConfigPA.SENS_RES[0] = 0x44;
     params.lmConfigPA.SENS_RES[1] = 0x00;
     params.lmConfigPA.SEL_RES = 0x00;
+    api_hal_nfc_exit_sleep();
 
     ReturnCode ret;
     ret = rfalNfcDiscover(&params);
@@ -204,6 +205,7 @@ void nfc_worker_emulate(NfcWorker* nfc_worker) {
     }
 
     rfalNfcDeactivate(false);
+    api_hal_nfc_start_sleep();
 }
 
 void nfc_worker_field(NfcWorker* nfc_worker) {
