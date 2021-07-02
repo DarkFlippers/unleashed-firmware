df9a6673da2e7a24bb4c0cd49fd339022ac6c007
Albert Kharisov <ah@bright-box.com>
2021-07-02 15:46:13 +0300

    [FL-1513] Fix IRDA crash (#557)

--- applications/irda/irda-app-transceiver.hpp
+++ applications/irda/irda-app-transceiver.hpp
@@ -12,6 +12,7 @@ public:
     void send_message(const IrdaMessage* message) const;
 
 private:
+    bool capture_started;
     osMessageQueueId_t event_queue;
     static void irda_rx_callback(void* ctx, bool level, uint32_t duration);
     IrdaHandler* decoder;
