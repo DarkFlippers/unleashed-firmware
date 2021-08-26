08f7a290a4188a770f53bae1b9f4e8a87d1fdaeb
Anna Prosvetova <anna@prosvetova.me>
2021-08-26 20:59:23 +0300

    CI: Fix git.mk & post a quick flash link on PR builds (#670)
    * CI: Fix git.mk & post a quick flash link on PR builds
    * Makefile: optimize shell invocation in git helper, conditional dependency load.
    * Github: better variable naming
    
    Co-authored-by: Aleksandr Kutuzov <alleteam@gmail.com>

--- make/git.mk
+++ make/git.mk
@@ -1,9 +1,13 @@
-GIT_COMMIT		= $(shell git describe --always  --exclude '*' || echo 'unknown')
-GIT_BRANCH		= $(shell git rev-parse --abbrev-ref HEAD || echo 'unknown')
-GIT_BRANCH_NUM	= $(shell git rev-list --count $(GIT_BRANCH) || echo 'nan')
-BUILD_DATE		= $(shell date '+%d-%m-%Y' || echo 'unknown')
-VERSION			= $(shell git describe --tags --abbrev=0 || echo 'unknown')
+GIT_COMMIT		:= $(shell git rev-parse --short HEAD || echo 'unknown')
+GIT_BRANCH		:= $(shell echo $${WORKFLOW_BRANCH_OR_TAG-$$(git rev-parse --abbrev-ref HEAD || echo 'unknown')})
+GIT_BRANCH_NUM	:= $(shell git rev-list --count HEAD || echo 'nan')
+BUILD_DATE		:= $(shell date '+%d-%m-%Y' || echo 'unknown')
+VERSION			:= $(shell git describe --tags --abbrev=0 --exact-match || echo 'unknown')
 
-CFLAGS			+= -DGIT_COMMIT="\"$(GIT_COMMIT)\"" -DGIT_BRANCH="\"$(GIT_BRANCH)\"" -DGIT_BRANCH_NUM="\"$(GIT_BRANCH_NUM)\""
-CFLAGS			+= -DBUILD_DATE="\"$(BUILD_DATE)\"" -DTARGET="\"$(TARGET)\"" -DVERSION="\"$(VERSION)\""
-
+CFLAGS += \
+	-DGIT_COMMIT="\"$(GIT_COMMIT)\"" \
+	-DGIT_BRANCH="\"$(GIT_BRANCH)\"" \
+	-DGIT_BRANCH_NUM="\"$(GIT_BRANCH_NUM)\"" \
+	-DBUILD_DATE="\"$(BUILD_DATE)\"" \
+	-DTARGET="\"$(TARGET)\"" \
+	-DVERSION="\"$(VERSION)\""
