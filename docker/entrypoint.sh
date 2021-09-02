94f161d1605062d1fb226218db587bf1610bc41b
Anna Prosvetova <anna@prosvetova.me>
2021-09-03 01:44:37 +0300

    Toolchain: Fix GDB (#686)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# A hack for GitHub Actions to not install Rust twice
-if [ "$HOME" != "/root" ]; then
-    ln -sf /root/.rustup "$HOME/.rustup"
-    ln -sf /root/.cargo "$HOME/.cargo"
-fi
-
-PATH="$HOME/.cargo/bin:${PATH}"
-
 if [ -z "$1" ]; then
     bash
 else
