6792e24b6ec59254f9b325f14ab4017be0db89c3
Skorpionm <85568270+Skorpionm@users.noreply.github.com>
2021-11-17 17:52:06 +0400

    SubGhz: Add new man key (#817)
    * SubGhz: fix kelog pattern
    * SubGhz: add Keloog Secure Learning
    * SubGhz: add man NICE_Smilo, NICE_MHOUSE, DEA, Genius, FAAC_RC,XT, Came_Space, DTM
    
    Co-authored-by: あく <alleteam@gmail.com>

--- lib/subghz/protocols/subghz_protocol_keeloq_common.h
+++ lib/subghz/protocols/subghz_protocol_keeloq_common.h
@@ -2,7 +2,6 @@
 #include "subghz_protocol_common.h"
 #include "file-worker.h"
 
-
 #include <furi.h>
 
 /*
@@ -12,19 +11,19 @@
  *
  */
 
-#define KEELOQ_NLF          0x3A5C742E
-#define bit(x,n)            (((x)>>(n))&1)
-#define g5(x,a,b,c,d,e)     (bit(x,a)+bit(x,b)*2+bit(x,c)*4+bit(x,d)*8+bit(x,e)*16)
+#define KEELOQ_NLF 0x3A5C742E
+#define bit(x, n) (((x) >> (n)) & 1)
+#define g5(x, a, b, c, d, e) \
+    (bit(x, a) + bit(x, b) * 2 + bit(x, c) * 4 + bit(x, d) * 8 + bit(x, e) * 16)
 
 /*
  * KeeLoq learning types
  * https://phreakerclub.com/forum/showthread.php?t=67
  */
-#define KEELOQ_LEARNING_UNKNOWN    0u
-#define KEELOQ_LEARNING_SIMPLE     1u
-#define KEELOQ_LEARNING_NORMAL     2u
-#define KEELOQ_LEARNING_SECURE     3u
-
+#define KEELOQ_LEARNING_UNKNOWN 0u
+#define KEELOQ_LEARNING_SIMPLE 1u
+#define KEELOQ_LEARNING_NORMAL 2u
+#define KEELOQ_LEARNING_SECURE 3u
 
 /** Simple Learning Encrypt
  * @param data - 0xBSSSCCCC, B(4bit) key, S(10bit) serial&0x3FF, C(16bit) counter
@@ -46,3 +45,13 @@ uint32_t subghz_protocol_keeloq_common_decrypt(const uint32_t data, const uint64
  * @return manufacture for this serial number (64bit)
  */
 uint64_t subghz_protocol_keeloq_common_normal_learning(uint32_t data, const uint64_t key);
+
+/** Secure Learning
+ * @param data - serial number (28bit)
+ * @param seed - serial number (32bit)
+ * @param key - manufacture (64bit)
+ * @return manufacture for this serial number (64bit)
+ */
+
+uint64_t
+    subghz_protocol_keeloq_common_secure_learning(uint32_t data, uint32_t seed, const uint64_t key);
