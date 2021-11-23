68274b6c2796243f93404ce59b0944023f7a2be5
Oleg Schwann <cup.of.software.code@gmail.com>
2021-11-23 19:07:46 +0300

    Add Snake game (#829)
    * Add snake game
    * Applications: Added a classic game https://en.wikipedia.org/wiki/Snake_(video_game_genre)
    * Snake Game: Making it impossible to lose button presses
    * Use more native press button event
    * Snake Game: use low level InputTypePress event instead of InputTypeShort high level unpredictable event
    
    Co-authored-by: LionZXY <nikita@kulikof.ru>
    Co-authored-by: あく <alleteam@gmail.com>

--- applications/applications.c
+++ applications/applications.c
@@ -42,6 +42,7 @@ extern int32_t vibro_test_app(void* p);
 
 // Plugins
 extern int32_t music_player_app(void* p);
+extern int32_t snake_game_app(void* p);
 
 // On system start hooks declaration
 extern void bt_cli_init();
@@ -202,6 +203,10 @@ const FlipperApplication FLIPPER_PLUGINS[] = {
 
 #ifdef APP_MUSIC_PLAYER
     {.app = music_player_app, .name = "Music Player", .stack_size = 1024, .icon = &A_Plugins_14},
+#endif
+
+#ifdef APP_SNAKE_GAME
+    {.app = snake_game_app, .name = "Snake Game", .stack_size = 1024, .icon = &A_Plugins_14},
 #endif
 };
 
