c4d1bb3ecaed1368b6cabb527831a2e8d00e92a4
r3df0xx <daniel@dogaru.org>
2022-04-15 20:35:13 +0300

    Seed input. Trying to create.

--- applications/subghz/helpers/subghz_custom_event.h
+++ applications/subghz/helpers/subghz_custom_event.h
@@ -13,6 +13,7 @@ typedef enum {
     SubGhzCustomEventSceneReceiverInfoTxStart,
     SubGhzCustomEventSceneReceiverInfoTxStop,
     SubGhzCustomEventSceneReceiverInfoSave,
+    SubGhzCustomEventByteInputDone,
     SubGhzCustomEventSceneSaveName,
     SubGhzCustomEventSceneSaveSuccess,
     SubGhzCustomEventSceneShowErrorBack,
