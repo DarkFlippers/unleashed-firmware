2e674fe95f690764cab7e7512e395118eb6d720b
Dmitry Pavlov <di-smash007@mail.ru>
2022-04-19 14:07:14 +0300

    [FL-2461] IR: change word from key to button (#1119)
    * 2461 ir renamed menu items
    * Renamed the key to the button
    
    Co-authored-by: あく <alleteam@gmail.com>

--- applications/infrared/scene/infrared_app_scene_edit_rename.cpp
+++ applications/infrared/scene/infrared_app_scene_edit_rename.cpp
@@ -14,7 +14,7 @@ void InfraredAppSceneEditRename::on_enter(InfraredApp* app) {
         strncpy(buffer_str, button_name.c_str(), max_len);
         buffer_str[max_len + 1] = 0;
         enter_name_length = max_len;
-        text_input_set_header_text(text_input, "Name the key");
+        text_input_set_header_text(text_input, "Name the button");
     } else {
         auto remote_name = remote_manager->get_remote_name();
         strncpy(app->get_text_store(0), remote_name.c_str(), app->get_text_store_size());
