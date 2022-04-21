e26c913d06b65fde7a08e10ced7f20aad6763799
r3df0xx <daniel@dogaru.org>
2022-04-21 19:55:08 +0300

    ---

--- applications/subghz/scenes/subghz_scene_set_fix.c
+++ applications/subghz/scenes/subghz_scene_set_fix.c
@@ -1,13 +1,13 @@
 #include "../subghz_i.h"
 
 void subghz_scene_set_fix_byte_input_callback(void* context) {
-    SubGhz* subghz = (SubGhz*)context;
+    SubGhz* subghz = context;
 
     view_dispatcher_send_custom_event(subghz->view_dispatcher, SubGhzCustomEventByteInputDone);
 }
 
 void subghz_scene_set_fix_on_enter(void* context) {
-    SubGhz* subghz = (SubGhz*)context;
+    SubGhz* subghz = context;
 
     // Setup view
     ByteInput* byte_input = subghz->byte_input;
@@ -23,19 +23,20 @@ void subghz_scene_set_fix_on_enter(void* context) {
 }
 
 bool subghz_scene_set_fix_on_event(void* context, SceneManagerEvent event) {
-    SubGhz* subghz = (SubGhz*)context;
+    SubGhz* subghz = context;
+    bool consumed = false;
 
     if(event.type == SceneManagerEventTypeCustom) {
         if(event.event == SubGhzCustomEventByteInputDone) {
             scene_manager_next_scene(subghz->scene_manager, SubGhzSceneSetCnt);
-            return true;
+            consumed = true;
         }
     }
-    return false;
+    return consumed;
 }
 
 void subghz_scene_set_fix_on_exit(void* context) {
-    SubGhz* subghz = (SubGhz*)context;
+    SubGhz* subghz = context;
 
     // Clear view
     byte_input_set_result_callback(subghz->byte_input, NULL, NULL, NULL, NULL, 0);
