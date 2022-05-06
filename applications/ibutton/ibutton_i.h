2b398133d0bc8fd8b1851a713b465ebf92b8b9d8
Georgii Surkov <37121527+gsurkov@users.noreply.github.com>
2022-05-06 19:48:39 +0300

    [FL-2514] Port iButton application to C (#1198)
    * Initial C iButton app setup
    * Add more scenes
    * Add even more scenes
    * Add even more scenes again
    * More scenes...
    * Add key info scene
    * Add delete success scene
    * Use scene state to store internal data
    * Add parameter parsing
    * Add emulate scene
    * Add write scene
    * Add write success scene
    * Add Read scene
    * Add read success scene
    * Add exit confirm scene
    * Add retry confirm scene
    * Add CRC error scene
    * Add not key scene
    * Add read key menu scene
    * Rename some scenes
    * Refactor conditionals
    * Remove unneeded custom events
    * Remove the old iButton app
    * Correct formatting
    * Remove rogue comments and function prototypes
    * iButton: cleanup merge artifacts and fix warnings
    
    Co-authored-by: Aleksandr Kutuzov <alleteam@gmail.com>

---
+++ applications/ibutton/ibutton_i.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include "ibutton.h"
+
+#include <gui/gui.h>
+#include <gui/view.h>
+#include <gui/view_dispatcher.h>
+#include <gui/scene_manager.h>
+#include <notification/notification_messages.h>
+
+#include <one_wire/ibutton/ibutton_worker.h>
+#include <storage/storage.h>
+#include <dialogs/dialogs.h>
+
+#include <gui/modules/submenu.h>
+#include <gui/modules/popup.h>
+#include <gui/modules/dialog_ex.h>
+#include <gui/modules/text_input.h>
+#include <gui/modules/byte_input.h>
+#include <gui/modules/widget.h>
+
+#include "ibutton_custom_event.h"
+#include "scenes/ibutton_scene.h"
+
+#define IBUTTON_FILE_NAME_SIZE 100
+#define IBUTTON_TEXT_STORE_SIZE 128
+
+#define IBUTTON_APP_FOLDER "/any/ibutton"
+#define IBUTTON_APP_EXTENSION ".ibtn"
+#define IBUTTON_APP_FILE_TYPE "Flipper iButton key"
+
+struct iButton {
+    SceneManager* scene_manager;
+    ViewDispatcher* view_dispatcher;
+
+    Gui* gui;
+    Storage* storage;
+    DialogsApp* dialogs;
+    NotificationApp* notifications;
+
+    iButtonWorker* key_worker;
+    iButtonKey* key;
+
+    char file_name[IBUTTON_FILE_NAME_SIZE];
+    char text_store[IBUTTON_TEXT_STORE_SIZE + 1];
+
+    Submenu* submenu;
+    ByteInput* byte_input;
+    TextInput* text_input;
+    Popup* popup;
+    Widget* widget;
+    DialogEx* dialog_ex;
+};
+
+typedef enum {
+    iButtonViewSubmenu,
+    iButtonViewByteInput,
+    iButtonViewTextInput,
+    iButtonViewPopup,
+    iButtonViewWidget,
+    iButtonViewDialogEx,
+} iButtonView;
+
+typedef enum {
+    iButtonNotificationMessageError,
+    iButtonNotificationMessageSuccess,
+    iButtonNotificationMessageRead,
+    iButtonNotificationMessageEmulate,
+    iButtonNotificationMessageYellowBlink,
+    iButtonNotificationMessageRedOn,
+    iButtonNotificationMessageRedOff,
+    iButtonNotificationMessageGreenOn,
+    iButtonNotificationMessageGreenOff,
+} iButtonNotificationMessage;
+
+bool ibutton_file_select(iButton* ibutton);
+bool ibutton_load_key(iButton* ibutton, const char* key_name);
+bool ibutton_save_key(iButton* ibutton, const char* key_name);
+bool ibutton_delete_key(iButton* ibutton);
+void ibutton_text_store_set(iButton* ibutton, const char* text, ...);
+void ibutton_text_store_clear(iButton* ibutton);
+void ibutton_switch_to_previous_scene_one_of(
+    iButton* ibutton,
+    const uint32_t* scene_ids,
+    size_t scene_ids_size);
+void ibutton_notification_message(iButton* ibutton, uint32_t message);
--- applications/ibutton/scene/ibutton_scene_select_key.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "ibutton_scene_generic.h"
-
-class iButtonSceneSelectKey : public iButtonScene {
-public:
-    void on_enter(iButtonApp* app) final;
-    bool on_event(iButtonApp* app, iButtonEvent* event) final;
-    void on_exit(iButtonApp* app) final;
-};
