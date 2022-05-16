9804f17ebe14dcc6841ccb90ae09825b7f511bcc
RogueMaster <RogueMaster@users.noreply.github.com>
2022-05-16 15:04:47 -0400

    Update random_name.c

--- lib/toolbox/random_name.c
+++ lib/toolbox/random_name.c
@@ -6,20 +6,13 @@
 #include <furi_hal.h>
 
 void set_random_name(char* name, uint8_t max_name_size) {
-    static bool rand_generator_inited = false;
-    if(!rand_generator_inited) {
-        srand(DWT->CYCCNT);
-        rand_generator_inited = true;
-    }
-
     FuriHalRtcDateTime datetime;
     furi_hal_rtc_get_datetime(&datetime);
     char strings[1][25];
-    sprintf(strings[0], "%s%.4d%.2d%.2d%.2d%.2d", "scan_"
+    sprintf(strings[0], "%s%.4d%.2d%.2d%.2d%.2d", "s"
         , datetime.year, datetime.month, datetime.day
         , datetime.hour, datetime.minute
     );
-
     sniprintf(name, max_name_size, "%s", strings[0]);
     // Set first symbol to upper case
     name[0] = name[0] - 0x20;
