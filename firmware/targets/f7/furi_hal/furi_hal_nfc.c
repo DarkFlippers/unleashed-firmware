b798a2855844c91735b7a166ebf482abf154030a
Gary <gary.reuter@gmail.com>
2022-05-26 09:00:59 -0400

    Change "FuriHalNfc Current state" trace to only log on change of state #1272
    Co-authored-by: gornekich <n.gorbadey@gmail.com>

--- firmware/targets/f7/furi_hal/furi_hal_nfc.c
+++ firmware/targets/f7/furi_hal/furi_hal_nfc.c
@@ -61,6 +61,7 @@ bool furi_hal_nfc_detect(FuriHalNfcDevData* nfc_data, uint32_t timeout) {
 
     rfalLowPowerModeStop();
     rfalNfcState state = rfalNfcGetState();
+    rfalNfcState state_old = 0;
     if(state == RFAL_NFC_STATE_NOTINIT) {
         rfalNfcInitialize();
     }
@@ -83,11 +84,14 @@ bool furi_hal_nfc_detect(FuriHalNfcDevData* nfc_data, uint32_t timeout) {
     while(true) {
         rfalNfcWorker();
         state = rfalNfcGetState();
+        if(state != state_old) {
+            FURI_LOG_T(TAG, "State change %d -> %d", state_old, state);
+        }
+        state_old = state;
         if(state == RFAL_NFC_STATE_ACTIVATED) {
             detected = true;
             break;
         }
-        FURI_LOG_T(TAG, "Current state %d", state);
         if(state == RFAL_NFC_STATE_POLL_ACTIVATION) {
             start = DWT->CYCCNT;
             continue;
