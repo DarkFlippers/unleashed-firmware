5b4e7f8dd124e350ebd721907a384f3756233ea4
Eng1n33r <101719414+Eng1n33r@users.noreply.github.com>
2022-07-02 17:03:48 +0300

    tc versioning from brunkbatya branch

--- fbt.cmd
+++ fbt.cmd
@@ -1,4 +1,5 @@
 @echo off
+set "flipper_toolchain_version=2"
 set "toolchainRoot=%~dp0toolchain\i686-windows"
 set "SCONS_DEFAULT_FLAGS=-Q --warn=target-not-built"
 if not exist "%~dp0.git" (
@@ -7,6 +8,13 @@ if not exist "%~dp0.git" (
 )
 git submodule update --init
 if not exist "%toolchainRoot%" (
-    powershell -ExecutionPolicy Bypass -File %~dp0scripts\toolchain\windows-toolchain-download.ps1
+    powershell -ExecutionPolicy Bypass -File %~dp0scripts\toolchain\windows-toolchain-download.ps1 "%flipper_toolchain_version%"
+)
+if not exist "%toolchainRoot%\VERSION" (
+    powershell -ExecutionPolicy Bypass -File %~dp0scripts\toolchain\windows-toolchain-download.ps1 "%flipper_toolchain_version%"
+)
+set /p real_toolchain_version=<%toolchainRoot%\VERSION
+if not "%real_toolchain_version%" == "%flipper_toolchain_version%" (
+    powershell -ExecutionPolicy Bypass -File %~dp0scripts\toolchain\windows-toolchain-download.ps1 "%flipper_toolchain_version%"
 )
 cmd /V /C "set "PATH=%toolchainRoot%\python;%toolchainRoot%\bin;%toolchainRoot%\protoc\bin;%toolchainRoot%\openocd\bin;%PATH%" && python lib\scons\scripts\scons.py %SCONS_DEFAULT_FLAGS% %*"
