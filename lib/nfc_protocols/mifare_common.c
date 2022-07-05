b6b9db4b521112c7067246f904e0d858d91cc971
Equip <72751518+equipter@users.noreply.github.com>
2022-07-05 16:34:21 +0100

    Added Javacard Emulated mifare classic 1K compatibility (#1369)
    * Add Mifare classic 1k JC handling
      Add mifare classic Javacard emulation handling
    * Adding MIFARE 1K Javacard Emulation Compatibility
      MIFARE Classic 1K Cards from NXP have the SAK value of 0x08.
      MIFARE Classic 1K Cards that are emulated via javacard applet have an SAK value of 0x09.
      Adding the SAK values accordingly so that Javacard emulated mifare classic tags are properly handled.
    * update mifare_common.c
      added javacard emulation handling for mifare classic 1k
    
    Co-authored-by: gornekich <n.gorbadey@gmail.com>
    Co-authored-by: あく <alleteam@gmail.com>

--- lib/nfc_protocols/mifare_common.c
+++ lib/nfc_protocols/mifare_common.c
@@ -6,7 +6,7 @@ MifareType mifare_common_get_type(uint8_t ATQA0, uint8_t ATQA1, uint8_t SAK) {
     if((ATQA0 == 0x44) && (ATQA1 == 0x00) && (SAK == 0x00)) {
         type = MifareTypeUltralight;
     } else if(
-        ((ATQA0 == 0x44 || ATQA0 == 0x04) && (SAK == 0x08 || SAK == 0x88)) ||
+        ((ATQA0 == 0x44 || ATQA0 == 0x04) && (SAK == 0x08 || SAK == 0x88 || SAK == 0x09)) ||
         ((ATQA0 == 0x42 || ATQA0 == 0x02) && (SAK == 0x18))) {
         type = MifareTypeClassic;
     } else if(ATQA0 == 0x44 && ATQA1 == 0x03 && SAK == 0x20) {
