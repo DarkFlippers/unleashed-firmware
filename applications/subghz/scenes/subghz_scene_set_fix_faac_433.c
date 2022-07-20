45f9f602e2be60b34b94e970d6233363f9ab3ff3
Eng1n33r <101719414+Eng1n33r@users.noreply.github.com>
2022-07-21 00:35:43 +0300

    Add FAAC SLH 433 MHz create support

--- applications/subghz/scenes/subghz_scene_set_fix_faac_433.c
+++ applications/subghz/scenes/subghz_scene_set_fix_faac_433.c
@@ -1,14 +1,14 @@
 #include "../subghz_i.h"
 
-#define TAG "SubGhzSetFix"
+#define TAG "SubGhzSetFixFaac433"
 
-void subghz_scene_set_fix_byte_input_callback(void* context) {
+void subghz_scene_set_fix_faac_433_byte_input_callback(void* context) {
     SubGhz* subghz = context;
 
     view_dispatcher_send_custom_event(subghz->view_dispatcher, SubGhzCustomEventByteInputDone);
 }
 
-void subghz_scene_set_fix_on_enter(void* context) {
+void subghz_scene_set_fix_faac_433_on_enter(void* context) {
     SubGhz* subghz = context;
 
     // Setup view
@@ -16,7 +16,7 @@ void subghz_scene_set_fix_on_enter(void* context) {
     byte_input_set_header_text(byte_input, "Enter FIX in hex");
     byte_input_set_result_callback(
         byte_input,
-        subghz_scene_set_fix_byte_input_callback,
+        subghz_scene_set_fix_faac_433_byte_input_callback,
         NULL,
         subghz,
         subghz->txrx->secure_data->fix,
@@ -24,20 +24,20 @@ void subghz_scene_set_fix_on_enter(void* context) {
     view_dispatcher_switch_to_view(subghz->view_dispatcher, SubGhzViewIdByteInput);
 }
 
-bool subghz_scene_set_fix_on_event(void* context, SceneManagerEvent event) {
+bool subghz_scene_set_fix_faac_433_on_event(void* context, SceneManagerEvent event) {
     SubGhz* subghz = context;
     bool consumed = false;
 
     if(event.type == SceneManagerEventTypeCustom) {
         if(event.event == SubGhzCustomEventByteInputDone) {
-            scene_manager_next_scene(subghz->scene_manager, SubGhzSceneSetCnt);
+            scene_manager_next_scene(subghz->scene_manager, SubGhzSceneSetCntFaac433);
             consumed = true;
         }
     }
     return consumed;
 }
 
-void subghz_scene_set_fix_on_exit(void* context) {
+void subghz_scene_set_fix_faac_433_on_exit(void* context) {
     SubGhz* subghz = context;
 
     // Clear view
