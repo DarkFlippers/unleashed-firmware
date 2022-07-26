c7772060657afc8ee5a160ea3ed62246d989c136
gornekich <n.gorbadey@gmail.com>
2022-07-26 20:28:37 +0300

    NFC: fix navigation from menu scenes #1459

--- applications/nfc/scenes/nfc_scene_mf_classic_menu.c
+++ applications/nfc/scenes/nfc_scene_mf_classic_menu.c
@@ -45,8 +45,7 @@ bool nfc_scene_mf_classic_menu_on_event(void* context, SceneManagerEvent event) 
             consumed = true;
         }
     } else if(event.type == SceneManagerEventTypeBack) {
-        consumed =
-            scene_manager_search_and_switch_to_previous_scene(nfc->scene_manager, NfcSceneStart);
+        consumed = scene_manager_previous_scene(nfc->scene_manager);
     }
 
     return consumed;
