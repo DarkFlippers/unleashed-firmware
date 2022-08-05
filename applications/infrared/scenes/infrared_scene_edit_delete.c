0a602e3791df0f25b0c7e5a6eaba745dde2ec809
Georgii Surkov <37121527+gsurkov@users.noreply.github.com>
2022-08-05 18:38:20 +0300

    [FL-2717] Fix unexpected behaviour when opening a remote from outside (#1538)
    * Fix unexpected behaviour when opening remote from outside
    * Same fix for editing button name
    * Exit application correctly if remote was deleted
    * Remove duplicate function from ibutton
    * Use COUNT_OF macro
    
    Co-authored-by: あく <alleteam@gmail.com>

--- applications/infrared/scenes/infrared_scene_edit_delete.c
+++ applications/infrared/scenes/infrared_scene_edit_delete.c
@@ -97,7 +97,7 @@ bool infrared_scene_edit_delete_on_event(void* context, SceneManagerEvent event)
             } else {
                 const uint32_t possible_scenes[] = {InfraredSceneRemoteList, InfraredSceneStart};
                 scene_manager_search_and_switch_to_previous_scene_one_of(
-                    scene_manager, possible_scenes, sizeof(possible_scenes) / sizeof(uint32_t));
+                    scene_manager, possible_scenes, COUNT_OF(possible_scenes));
             }
             consumed = true;
         }
