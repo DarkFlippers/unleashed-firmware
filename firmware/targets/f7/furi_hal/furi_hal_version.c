6ee2c15854c3f29490697cdf85388ae5754eeba3
あく <alleteam@gmail.com>
2022-08-12 19:19:29 +0900

    FuriHal,About,Desktop: show provisioned region (#1589)
    Co-authored-by: hedger <hedger@users.noreply.github.com>

--- firmware/targets/f7/furi_hal/furi_hal_version.c
+++ firmware/targets/f7/furi_hal/furi_hal_version.c
@@ -262,6 +262,8 @@ const char* furi_hal_version_get_hw_region_name() {
         return "R02";
     case FuriHalVersionRegionJp:
         return "R03";
+    case FuriHalVersionRegionWorld:
+        return "R04";
     }
     return "R??";
 }
