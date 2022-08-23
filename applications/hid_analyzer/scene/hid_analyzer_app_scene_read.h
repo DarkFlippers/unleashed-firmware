e132d1efe77b86c655cbd9bf1458c6f9cac37259
MX <10697207+xMasterX@users.noreply.github.com>
2022-08-23 19:14:30 +0300

    added multiconverter / removed hid analyzer
    hid analyzer won't needed anymore because of new lfrfid system adds support for multiple hid protocols

--- applications/hid_analyzer/scene/hid_analyzer_app_scene_read.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../hid_analyzer_app.h"
-
-class HIDAppSceneRead : public GenericScene<HIDApp> {
-public:
-    void on_enter(HIDApp* app, bool need_restore) final;
-    bool on_event(HIDApp* app, HIDApp::Event* event) final;
-    void on_exit(HIDApp* app) final;
-};
