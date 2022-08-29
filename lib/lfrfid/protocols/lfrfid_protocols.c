d76ba206522346763c4e7cfdb37607c9faf1e969
Sebastian Mauer <mauimauer@gmail.com>
2022-08-29 17:31:28 +0100

    Add support for Pyramid tags (#1676)
    * Add support for Pyramid tags
    * Also add additional checks for AWID decoder to avoid missdetection
    
    * lfrfid worker: reset GPIO_LOAD pin
    * lfrfid: protocol viking, format
    * lfrfid: protocol pyramid, format
    * lfrfid: protocol paradox, format
    * lfrfid: protocol jablotron, format
    * lfrfid: protocol em4100, format
    * lfrfid: increase reading time by 0.5s since protocol viking takes longer to read
    
    Co-authored-by: SG <who.just.the.doctor@gmail.com>

--- lib/lfrfid/protocols/lfrfid_protocols.c
+++ lib/lfrfid/protocols/lfrfid_protocols.c
@@ -8,6 +8,7 @@
 #include "protocol_fdx_b.h"
 #include "protocol_hid_generic.h"
 #include "protocol_hid_ex_generic.h"
+#include "protocol_pyramid.h"
 #include "protocol_viking.h"
 #include "protocol_jablotron.h"
 #include "protocol_paradox.h"
@@ -23,6 +24,7 @@ const ProtocolBase* lfrfid_protocols[] = {
     [LFRFIDProtocolFDXB] = &protocol_fdx_b,
     [LFRFIDProtocolHidGeneric] = &protocol_hid_generic,
     [LFRFIDProtocolHidExGeneric] = &protocol_hid_ex_generic,
+    [LFRFIDProtocolPyramid] = &protocol_pyramid,
     [LFRFIDProtocolViking] = &protocol_viking,
     [LFRFIDProtocolJablotron] = &protocol_jablotron,
     [LFRFIDProtocolParadox] = &protocol_paradox,
--- lib/lfrfid/protocols/protocol_viking.c
+++ lib/lfrfid/protocols/protocol_viking.c
@@ -173,7 +173,7 @@ bool protocol_viking_write_data(ProtocolViking* protocol, void* data) {
 
 void protocol_viking_render_data(ProtocolViking* protocol, string_t result) {
     uint32_t id = bit_lib_get_bits_32(protocol->data, 0, 32);
-    string_printf(result, "ID: %08lx\r\n", id);
+    string_printf(result, "ID: %08lX\r\n", id);
 };
 
 const ProtocolBase protocol_viking = {
