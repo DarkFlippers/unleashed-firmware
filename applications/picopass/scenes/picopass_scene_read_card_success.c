5c735f031357bc8152bbef976ef3e64ccfcb76f9
Eric Betts <bettse@fastmail.fm>
2022-08-30 06:59:34 -0700

    Picopass write (#1658)
    * [picopass] Prevent false success with non-standard key
    * UI for writing
    * worker function for write
    * Update write command value
    * Show card read failure message
    
    Co-authored-by: あく <alleteam@gmail.com>

--- applications/picopass/scenes/picopass_scene_read_card_success.c
+++ applications/picopass/scenes/picopass_scene_read_card_success.c
@@ -29,38 +29,57 @@ void picopass_scene_read_card_success_on_enter(void* context) {
     PicopassPacs* pacs = &picopass->dev->dev_data.pacs;
     Widget* widget = picopass->widget;
 
-    size_t bytesLength = 1 + pacs->record.bitLength / 8;
-    string_set_str(credential_str, "");
-    for(uint8_t i = PICOPASS_BLOCK_LEN - bytesLength; i < PICOPASS_BLOCK_LEN; i++) {
-        string_cat_printf(credential_str, " %02X", pacs->credential[i]);
-    }
+    if(pacs->record.bitLength == 0) {
+        string_cat_printf(wiegand_str, "Read Failed");
 
-    if(pacs->record.valid) {
-        string_cat_printf(
-            wiegand_str, "FC: %u CN: %u", pacs->record.FacilityCode, pacs->record.CardNumber);
+        widget_add_button_element(
+            widget,
+            GuiButtonTypeLeft,
+            "Retry",
+            picopass_scene_read_card_success_widget_callback,
+            picopass);
+
+        widget_add_string_element(
+            widget, 64, 12, AlignCenter, AlignCenter, FontPrimary, string_get_cstr(wiegand_str));
     } else {
-        string_cat_printf(wiegand_str, "%d bits", pacs->record.bitLength);
+        size_t bytesLength = 1 + pacs->record.bitLength / 8;
+        string_set_str(credential_str, "");
+        for(uint8_t i = PICOPASS_BLOCK_LEN - bytesLength; i < PICOPASS_BLOCK_LEN; i++) {
+            string_cat_printf(credential_str, " %02X", pacs->credential[i]);
+        }
+
+        if(pacs->record.valid) {
+            string_cat_printf(
+                wiegand_str, "FC: %u CN: %u", pacs->record.FacilityCode, pacs->record.CardNumber);
+        } else {
+            string_cat_printf(wiegand_str, "%d bits", pacs->record.bitLength);
+        }
+
+        widget_add_button_element(
+            widget,
+            GuiButtonTypeLeft,
+            "Retry",
+            picopass_scene_read_card_success_widget_callback,
+            picopass);
+
+        widget_add_button_element(
+            widget,
+            GuiButtonTypeRight,
+            "More",
+            picopass_scene_read_card_success_widget_callback,
+            picopass);
+
+        widget_add_string_element(
+            widget, 64, 12, AlignCenter, AlignCenter, FontPrimary, string_get_cstr(wiegand_str));
+        widget_add_string_element(
+            widget,
+            64,
+            32,
+            AlignCenter,
+            AlignCenter,
+            FontSecondary,
+            string_get_cstr(credential_str));
     }
-
-    widget_add_button_element(
-        widget,
-        GuiButtonTypeLeft,
-        "Retry",
-        picopass_scene_read_card_success_widget_callback,
-        picopass);
-
-    widget_add_button_element(
-        widget,
-        GuiButtonTypeRight,
-        "More",
-        picopass_scene_read_card_success_widget_callback,
-        picopass);
-
-    widget_add_string_element(
-        widget, 64, 12, AlignCenter, AlignCenter, FontPrimary, string_get_cstr(wiegand_str));
-    widget_add_string_element(
-        widget, 64, 32, AlignCenter, AlignCenter, FontSecondary, string_get_cstr(credential_str));
-
     string_clear(credential_str);
     string_clear(wiegand_str);
 
