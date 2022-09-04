6f77f537e337642b2c4bb9dfe73f0d72d97ada9c
Ganapati <ganapati_sec@protonmail.com>
2022-09-04 23:32:58 +0200

    Fi memory issues ad add new attack

--- applications/flipfrid/scene/flipfrid_scene_entrypoint.c
+++ applications/flipfrid/scene/flipfrid_scene_entrypoint.c
@@ -3,7 +3,6 @@
 string_t menu_items[4];
 
 void flipfrid_scene_entrypoint_menu_callback(FlipFridState* context, uint32_t index) {
-    FURI_LOG_D(TAG, "MENU: %d", index);
     switch(index) {
     case FlipFridAttackDefaultValues:
         context->attack = FlipFridAttackDefaultValues;
@@ -20,25 +19,38 @@ void flipfrid_scene_entrypoint_menu_callback(FlipFridState* context, uint32_t in
         context->current_scene = SceneSelectFile;
         string_set_str(context->attack_name, "Load File");
         break;
+    case FlipFridAttackLoadFileCustomUids:
+        context->attack = FlipFridAttackLoadFileCustomUids;
+        context->current_scene = SceneLoadCustomUids;
+        string_set_str(context->attack_name, "Load Custom UIDs");
+        break;
     default:
         break;
     }
 }
 
 void flipfrid_scene_entrypoint_on_enter(FlipFridState* context) {
+    // Clear the previous payload
+    context->payload[0] = 0x00;
+    context->payload[1] = 0x00;
+    context->payload[2] = 0x00;
+    context->payload[3] = 0x00;
+    context->payload[4] = 0x00;
+
     context->menu_index = 0;
-    for(uint32_t i = 0; i < 3; i++) {
+    for(uint32_t i = 0; i < 4; i++) {
         string_init(menu_items[i]);
     }
 
     string_set(menu_items[0], "Default Values");
     string_set(menu_items[1], "BF Customer ID");
     string_set(menu_items[2], "Load File");
+    string_set(menu_items[3], "Load uids from file");
 }
 
 void flipfrid_scene_entrypoint_on_exit(FlipFridState* context) {
     UNUSED(context);
-    for(uint32_t i = 0; i < 3; i++) {
+    for(uint32_t i = 0; i < 4; i++) {
         string_clear(menu_items[i]);
     }
 }
@@ -52,7 +64,7 @@ void flipfrid_scene_entrypoint_on_event(FlipFridEvent event, FlipFridState* cont
         if(event.input_type == InputTypeShort) {
             switch(event.key) {
             case InputKeyDown:
-                if(context->menu_index < FlipFridAttackLoadFile) {
+                if(context->menu_index < FlipFridAttackLoadFileCustomUids) {
                     context->menu_index++;
                 }
                 break;
@@ -98,7 +110,7 @@ void flipfrid_scene_entrypoint_on_draw(Canvas* canvas, FlipFridState* context) {
     canvas_draw_str_aligned(
         canvas, 64, 36, AlignCenter, AlignTop, string_get_cstr(menu_items[context->menu_index]));
 
-    if(context->menu_index < FlipFridAttackLoadFile) {
+    if(context->menu_index < FlipFridAttackLoadFileCustomUids) {
         canvas_set_font(canvas, FontSecondary);
         canvas_draw_str_aligned(
             canvas,
