6691e0529a897f31d59cc7db6a34548f1c19cdbd
Ganapati <ganapati_sec@protonmail.com>
2022-09-07 15:39:04 +0200

    add tx region check

--- applications/subbrute/subbrute_utils.c
+++ applications/subbrute/subbrute_utils.c
@@ -4,8 +4,7 @@ bool subbrute_is_frequency_allowed(SubBruteState* context) {
     // I know you don't like it but laws are laws
     // It's opensource so do whatever you want, but remember the risks :)
     // (Yes, this comment is the only purpose of this function)
-    //bool r = furi_hal_region_is_frequency_allowed(context->frequency);
-    bool r = true;
+    bool r = furi_hal_subghz_is_tx_allowed(context->frequency);
     if(!r) {
         FURI_LOG_E(TAG, "Frequency %d is not allowed in your region", context->frequency);
         notification_message(context->notify, &sequence_single_vibro);
