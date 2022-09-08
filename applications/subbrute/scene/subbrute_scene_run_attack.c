9d7edd206bb06204163f2b07a58413847447007c
MX <10697207+xMasterX@users.noreply.github.com>
2022-09-08 19:53:02 +0300

    change save gui in brute

--- applications/subbrute/scene/subbrute_scene_run_attack.c
+++ applications/subbrute/scene/subbrute_scene_run_attack.c
@@ -277,11 +277,12 @@ void subbrute_scene_run_attack_on_event(SubBruteEvent event, SubBruteState* cont
         if(event.input_type == InputTypeShort) {
             switch(event.key) {
             case InputKeyDown:
+                break;
+            case InputKeyUp:
                 if(!context->is_attacking) {
                     toSave = true;
                     context->current_scene = SceneSaveName;
                 }
-            case InputKeyUp:
                 break;
             case InputKeyLeft:
                 if(!context->is_attacking && context->payload > 0x00) {
@@ -354,8 +355,8 @@ void subbrute_scene_run_attack_on_draw(Canvas* canvas, SubBruteState* context) {
     canvas_draw_str_aligned(canvas, 64, 24, AlignCenter, AlignTop, msg_index);
 
     canvas_set_font(canvas, FontSecondary);
-    char start_stop_msg[20];
-    snprintf(start_stop_msg, sizeof(start_stop_msg), " Press (V) to save ");
+    char start_stop_msg[23];
+    snprintf(start_stop_msg, sizeof(start_stop_msg), " Press (^) to save ");
     if(context->is_attacking) {
         elements_button_center(canvas, "Stop");
     } else {
