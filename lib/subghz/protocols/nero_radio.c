24ecd6261873446f850147cd077d61b1f705a98b
Max Lapan <max.lapan@gmail.com>
2022-09-20 07:29:10 +0200

    Subghz: Adding checks for get_upload functions (#1704)
    * Adding checks for get_upload functions
      Almost in every protocol, function which generates upload might fail and return false.
      But we don't check this result, which might end up sending random memory contents to the air.
    * Format sources and fix crash on ivalid bit count in chamberlain
    
    Co-authored-by: あく <alleteam@gmail.com>

--- lib/subghz/protocols/nero_radio.c
+++ lib/subghz/protocols/nero_radio.c
@@ -172,7 +172,7 @@ bool subghz_protocol_encoder_nero_radio_deserialize(void* context, FlipperFormat
         flipper_format_read_uint32(
             flipper_format, "Repeat", (uint32_t*)&instance->encoder.repeat, 1);
 
-        subghz_protocol_encoder_nero_radio_get_upload(instance);
+        if(!subghz_protocol_encoder_nero_radio_get_upload(instance)) break;
         instance->encoder.is_running = true;
 
         res = true;
