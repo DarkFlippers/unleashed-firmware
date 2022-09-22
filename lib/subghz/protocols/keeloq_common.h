35eda9df38fa456a365c920beb7c92247d673014
MX <10697207+xMasterX@users.noreply.github.com>
2022-09-22 18:01:54 +0300

    keeeloq update

--- lib/subghz/protocols/keeloq_common.h
+++ lib/subghz/protocols/keeloq_common.h
@@ -22,6 +22,7 @@
 #define KEELOQ_LEARNING_SECURE 3u
 #define KEELOQ_LEARNING_MAGIC_XOR_TYPE_1 4u
 #define KEELOQ_LEARNING_FAAC 5u
+#define KEELOQ_LEARNING_MAGIC_SERIAL_TYPE_1 6u
 
 /**
  * Simple Learning Encrypt
@@ -71,3 +72,11 @@ uint64_t subghz_protocol_keeloq_common_magic_xor_type1_learning(uint32_t data, u
  * @return man_learning for this fix number (64bit)
  */
 uint64_t subghz_protocol_keeloq_common_faac_learning(const uint32_t seed, const uint64_t key);
+
+/** Magic_serial_type1 Learning
+ * @param data - serial number (28bit)
+ * @param man - magic man (64bit)
+ * @return manufacture for this serial number (64bit)
+ */
+
+uint64_t subghz_protocol_keeloq_common_magic_serial_type1_learning(uint32_t data, uint64_t man);
