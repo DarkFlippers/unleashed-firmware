d886e57bd8d9ec4cb12f8de7d391652157102fa4
MX <10697207+xMasterX@users.noreply.github.com>
2022-09-22 21:51:47 +0300

    remove duplicate function, update changelog

--- applications/main/infrared/infrared_brute_force.c
+++ applications/main/infrared/infrared_brute_force.c
@@ -157,7 +157,3 @@ void infrared_brute_force_add_record(
     InfraredBruteForceRecordDict_set_at(brute_force->records, key, value);
     string_clear(key);
 }
-
-void infrared_brute_force_reset(InfraredBruteForce* brute_force) {
-    InfraredBruteForceRecordDict_reset(brute_force->records);
-}
