3de6cf365c2f9344ee6fc86af07270f502ee2b3c
MX <10697207+xMasterX@users.noreply.github.com>
2022-09-28 06:02:23 +0300

    return fix

--- applications/main/subghz/subghz_last_settings.c
+++ applications/main/subghz/subghz_last_settings.c
@@ -7,7 +7,7 @@
 
 #define SUBGHZ_LAST_SETTING_FILE_TYPE "Flipper SubGhz Last Setting File"
 #define SUBGHZ_LAST_SETTING_FILE_VERSION 1
-#define SUBGHZ_LAST_SETTINGS_PATH EXT_PATH(".subghz.settings")
+#define SUBGHZ_LAST_SETTINGS_PATH EXT_PATH("subghz/assets/.subghz.settings")
 // 1 = "AM650"
 // "AM270", "AM650", "FM238", "FM476",
 #define SUBGHZ_LAST_SETTING_DEFAULT_PRESET 1
