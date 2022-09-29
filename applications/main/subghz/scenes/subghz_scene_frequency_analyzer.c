db30e52a9f272220b378ee2517e2623e59d8f30e
derskythe <skif@skif.ws>
2022-09-29 23:46:54 +0400

    Long press OK button in SubGHz Frequency analyzer switch to Read menu with selected frequency

--- applications/main/subghz/scenes/subghz_scene_frequency_analyzer.c
+++ applications/main/subghz/scenes/subghz_scene_frequency_analyzer.c
@@ -1,6 +1,8 @@
 #include "../subghz_i.h"
 #include <dolphin/dolphin.h>
 
+#define TAG "SubGhzSceneFrequencyAnalyzer"
+
 void subghz_scene_frequency_analyzer_callback(SubGhzCustomEvent event, void* context) {
     furi_assert(context);
     SubGhz* subghz = context;
@@ -17,16 +19,24 @@ void subghz_scene_frequency_analyzer_on_enter(void* context) {
 
 bool subghz_scene_frequency_analyzer_on_event(void* context, SceneManagerEvent event) {
     SubGhz* subghz = context;
-    if(event.type == SceneManagerEventTypeCustom &&
-       event.event == SubGhzCustomEventViewReceiverOK) {
-        uint32_t frequency =
-            subghz_frequency_analyzer_get_frequency_to_save(subghz->subghz_frequency_analyzer);
-        if(frequency > 0) {
-            subghz->last_settings->frequency = frequency;
-            subghz_last_settings_save(subghz->last_settings);
-        }
+    if(event.type == SceneManagerEventTypeCustom) {
+        if(event.event == SubGhzCustomEventViewReceiverOK) {
+            uint32_t frequency =
+                subghz_frequency_analyzer_get_frequency_to_save(subghz->subghz_frequency_analyzer);
+            if(frequency > 0) {
+                subghz->last_settings->frequency = frequency;
+                subghz_last_settings_save(subghz->last_settings);
+            }
 
-        return true;
+            return true;
+        } else if(event.event == SubGhzCustomEventViewReceiverUnlock) {
+            // Don't need to save, we already saved on short event
+#if FURI_DEBUG
+            FURI_LOG_W(TAG, "Goto next scene!");
+#endif
+            scene_manager_next_scene(subghz->scene_manager, SubGhzSceneReceiver);
+            return true;
+        }
     }
     return false;
 }
