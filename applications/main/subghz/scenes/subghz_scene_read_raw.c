c3d2b13da312c12a768bffea2ff7c9d7b190cb03
MX <10697207+xMasterX@users.noreply.github.com>
2022-10-02 08:50:41 +0300

    make loading subghz file from favourites a bit faster

--- applications/main/subghz/scenes/subghz_scene_read_raw.c
+++ applications/main/subghz/scenes/subghz_scene_read_raw.c
@@ -134,12 +134,22 @@ bool subghz_scene_read_raw_on_event(void* context, SceneManagerEvent event) {
                 scene_manager_next_scene(subghz->scene_manager, SubGhzSceneNeedSaving);
             } else {
                 //Restore default setting
-                subghz_preset_init(
-                    subghz,
-                    subghz_setting_get_preset_name(subghz->setting, subghz->last_settings->preset),
-                    subghz->last_settings->frequency,
-                    NULL,
-                    0);
+                if(subghz->raw_send_only) {
+                    subghz_preset_init(
+                        subghz,
+                        "AM650",
+                        subghz_setting_get_default_frequency(subghz->setting),
+                        NULL,
+                        0);
+                } else {
+                    subghz_preset_init(
+                        subghz,
+                        subghz_setting_get_preset_name(
+                            subghz->setting, subghz->last_settings->preset),
+                        subghz->last_settings->frequency,
+                        NULL,
+                        0);
+                }
                 if(!scene_manager_search_and_switch_to_previous_scene(
                        subghz->scene_manager, SubGhzSceneSaved)) {
                     if(!scene_manager_search_and_switch_to_previous_scene(
