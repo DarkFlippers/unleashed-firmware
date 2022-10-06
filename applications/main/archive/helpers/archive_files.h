2ee71f7b89f8aec1673d0369da964348d0819426
Max Andreev <drunkbatya.js@gmail.com>
2022-10-06 20:37:53 +0500

    ".fap" extention in file browser and archive tab (#1812)
    * Add .fap extention, and Applications tab
    * Using new icon, renaming tab to Apps
    * Change tabs order
    * Add first ugly implementation of in-app icons in archive browser
    * Starting using FAPLoader callback
    * Getting all metafata from fap
    * add app filename fallback
    * using fap_loader_item_callback in archive_list_item_cb
    * FAP-Loader: removed minimal allocation
    * Removed strange code
    
    Co-authored-by: SG <who.just.the.doctor@gmail.com>
    Co-authored-by: あく <alleteam@gmail.com>

--- applications/main/archive/helpers/archive_files.h
+++ applications/main/archive/helpers/archive_files.h
@@ -4,6 +4,8 @@
 #include <furi.h>
 #include <storage/storage.h>
 
+#define FAP_MANIFEST_MAX_ICON_SIZE 32
+
 typedef enum {
     ArchiveFileTypeIButton,
     ArchiveFileTypeNFC,
@@ -13,6 +15,7 @@ typedef enum {
     ArchiveFileTypeBadUsb,
     ArchiveFileTypeU2f,
     ArchiveFileTypeUpdateManifest,
+    ArchiveFileTypeApplication,
     ArchiveFileTypeFolder,
     ArchiveFileTypeUnknown,
     ArchiveFileTypeLoading,
@@ -21,33 +24,56 @@ typedef enum {
 typedef struct {
     FuriString* path;
     ArchiveFileTypeEnum type;
+    uint8_t* custom_icon_data;
+    FuriString* custom_name;
     bool fav;
     bool is_app;
 } ArchiveFile_t;
 
 static void ArchiveFile_t_init(ArchiveFile_t* obj) {
-    obj->type = ArchiveFileTypeUnknown;
-    obj->is_app = false;
-    obj->fav = false;
     obj->path = furi_string_alloc();
+    obj->type = ArchiveFileTypeUnknown;
+    obj->custom_icon_data = NULL;
+    obj->custom_name = furi_string_alloc();
+    obj->fav = false;
+    obj->is_app = false;
 }
 
 static void ArchiveFile_t_init_set(ArchiveFile_t* obj, const ArchiveFile_t* src) {
-    obj->type = src->type;
-    obj->is_app = src->is_app;
-    obj->fav = src->fav;
     obj->path = furi_string_alloc_set(src->path);
+    obj->type = src->type;
+    if(src->custom_icon_data) {
+        obj->custom_icon_data = malloc(FAP_MANIFEST_MAX_ICON_SIZE);
+        memcpy(obj->custom_icon_data, src->custom_icon_data, FAP_MANIFEST_MAX_ICON_SIZE);
+    } else {
+        obj->custom_icon_data = NULL;
+    }
+    obj->custom_name = furi_string_alloc_set(src->custom_name);
+    obj->fav = src->fav;
+    obj->is_app = src->is_app;
 }
 
 static void ArchiveFile_t_set(ArchiveFile_t* obj, const ArchiveFile_t* src) {
-    obj->type = src->type;
-    obj->is_app = src->is_app;
-    obj->fav = src->fav;
     furi_string_set(obj->path, src->path);
+    obj->type = src->type;
+    if(src->custom_icon_data) {
+        obj->custom_icon_data = malloc(FAP_MANIFEST_MAX_ICON_SIZE);
+        memcpy(obj->custom_icon_data, src->custom_icon_data, FAP_MANIFEST_MAX_ICON_SIZE);
+    } else {
+        obj->custom_icon_data = NULL;
+    }
+    furi_string_set(obj->custom_name, src->custom_name);
+    obj->fav = src->fav;
+    obj->is_app = src->is_app;
 }
 
 static void ArchiveFile_t_clear(ArchiveFile_t* obj) {
     furi_string_free(obj->path);
+    if(obj->custom_icon_data) {
+        free(obj->custom_icon_data);
+        obj->custom_icon_data = NULL;
+    }
+    furi_string_free(obj->custom_name);
 }
 
 ARRAY_DEF(
