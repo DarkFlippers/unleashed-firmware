52361b4adf8ca9614321fa21af2960ac60469c20
DerSkythe <skif@skif.ws>
2022-10-10 20:20:56 +0400

    Add split RAW files ability

--- applications/main/subghz/subghz_history_private.h
+++ applications/main/subghz/subghz_history_private.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "subghz_history.h"
+#include <toolbox/stream/stream.h>
 
 /**
  * @brief Generate filename like 000.tmp
@@ -52,7 +53,7 @@ void subghz_history_item_free(void* current_item);
 void subghz_history_clean_item_array(SubGhzHistory* instance);
 
 /**
- * @brief Write temp file fully, without spliting
+ * @brief Write temp file fully, without splitting
  * 
  * @param instance - SubGhzHistory*
  * @param current_item - SubGhzHistoryItem*
@@ -64,15 +65,99 @@ void subghz_history_tmp_write_file_full(
     FuriString* dir_path);
 
 /**
- * @brief Write temp splited to lines
+ * @brief Write temp spited to lines
  * 
  * @param instance - SubGhzHistory*
  * @param current_item - SubGhzHistoryItem*
  * @param dir_path - full path to file
  * @return true - file saved
- * @return false - error occured
+ * @return false - error occurred
  */
 bool subghz_history_tmp_write_file_split(
     SubGhzHistory* instance,
     void* current_item,
-    FuriString* dir_path);
+    const char* dir_path);
+
+/**
+ * @brief generate random value
+ * 
+ * @param min - min value
+ * @param max - max value
+ * @return uint32_t 
+ */
+uint32_t subghz_history_rand_range(uint32_t min, uint32_t max);
+
+/**
+ * @brief write random noise signals to file applying to max line value
+ * 
+ * @param file - Stream*
+ * @param is_negative_start - first value is negative or positive?
+ * @param current_position - 0 if started from begining
+ * @param empty_line - add RAW_Data to this line
+ * @return true 
+ * @return false 
+ */
+bool subghz_history_write_file_noise(
+    Stream* file,
+    bool is_negative_start,
+    size_t current_position,
+    bool empty_line);
+
+/**
+ * @brief taken from flipper_format_stream_read_value_line but takes only one int32 value
+ * 
+ * @param stream - Stream*
+ * @param _data - int32_t* output data
+ * @param data_size - size of data
+ * @return true 
+ * @return false 
+ */
+bool subghz_history_read_int32(Stream* stream, int32_t* _data, const uint16_t data_size);
+
+/**
+ * @brief write payload to file spliting by lines
+ * 
+ * @param src - Stream* of source
+ * @param file - Stream* of file
+ * @param is_negative_start - first value is negative or positive?
+ * @param current_position - by default is 0 but in this value returned last position of payload
+ * @return true 
+ * @return false 
+ */
+bool subghz_history_write_file_data(
+    Stream* src,
+    Stream* file,
+    bool* is_negative_start,
+    size_t* current_position);
+
+/**
+ * @brief taken from flipper_format_stream_read_valid_key
+ * 
+ * @param stream - Stream*
+ * @param key - FuriString* output value
+ * @return true 
+ * @return false 
+ */
+bool subghz_history_stream_read_valid_key(Stream* stream, FuriString* key);
+
+/**
+ * @brief taken from flipper_format_stream_seek_to_key
+ * 
+ * @param stream  - Stream*
+ * @param key - key
+ * @param strict_mode - false 
+ * @return true 
+ * @return false 
+ */
+bool subghz_history_stream_seek_to_key(Stream* stream, const char* key, bool strict_mode);
+
+/**
+ * @brief taken from flipper_format_stream_read_value
+ * 
+ * @param stream - Stream*
+ * @param value - FuriString* output value
+ * @param last - return position is last flag
+ * @return true 
+ * @return false 
+ */
+bool subghz_history_stream_read_value(Stream* stream, FuriString* value, bool* last);
