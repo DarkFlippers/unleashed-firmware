db2f4847bae956d760758eaae41805c605af81a9
hedger <hedger@nanode.su>
2022-10-12 23:13:17 +0400

    scriptsL simpler tar format

--- scripts/sconsdist.py
+++ scripts/sconsdist.py
@@ -170,6 +170,7 @@ class Main(App):
                     ),
                     "w:gz",
                     compresslevel=9,
+                    format=tarfile.USTAR_FORMAT,
                 ) as tar:
                     tar.add(bundle_dir, arcname=bundle_dir_name)
 
