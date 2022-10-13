39cd10f061425a165e06581bad674e62c5ee3b78
an4tur0r <evil.rodriges@gmail.com>
2022-10-13 16:54:13 +0300

    lock device shortcut

--- applications/services/desktop/scenes/desktop_scene_main.c
+++ applications/services/desktop/scenes/desktop_scene_main.c
@@ -9,6 +9,7 @@
 #include "../views/desktop_view_main.h"
 #include "desktop_scene.h"
 #include "desktop_scene_i.h"
+#include "../helpers/pin_lock.h"
 
 #define TAG "DesktopSrv"
 
@@ -93,6 +94,18 @@ bool desktop_scene_main_on_event(void* context, SceneManagerEvent event) {
 
         case DesktopMainEventOpenDebug:
             scene_manager_next_scene(desktop->scene_manager, DesktopSceneDebug);
+            consumed = true;
+            break;
+
+        case DesktopMainEventLock:
+            if(desktop->settings.pin_code.length > 0) {
+                scene_manager_set_scene_state(desktop->scene_manager, DesktopSceneLockMenu, 1);
+                desktop_pin_lock(&desktop->settings);
+                desktop_lock(desktop);
+            } else {
+                scene_manager_set_scene_state(desktop->scene_manager, DesktopSceneLockMenu, 0);
+                desktop_lock(desktop);
+            }
             consumed = true;
             break;
 
