378bf050684b9e56d71c84fd2e44a57caa507ee2
Skorpionm <85568270+Skorpionm@users.noreply.github.com>
2022-10-26 18:56:54 +0400

    [FL-2934] WS: add protocol Acurite 592TXR (#1916)
    * WS: add protocol Acurite 592TXR
    * WS: fix decoder Acurite_592TXR
    * SubGhz: proper types in math
    
    Co-authored-by: Aleksandr Kutuzov <alleteam@gmail.com>

--- applications/plugins/weather_station/protocols/protocol_items.h
+++ applications/plugins/weather_station/protocols/protocol_items.h
@@ -8,5 +8,6 @@
 #include "acurite_606tx.h"
 #include "lacrosse_tx141thbv2.h"
 #include "oregon2.h"
+#include "acurite_592txr.h"
 
 extern const SubGhzProtocolRegistry weather_station_protocol_registry;
