aa2ecbe80f77d6b7132cd4fdb83eb49fba297b31
Samuel Stauffer <samuel@descolada.com>
2022-11-07 06:38:04 -0800

    infrared: add Kaseikyo IR protocol (#1965)
    * infrared: add Kaseikyo IR protocol
    
    Add Kaseikyo IR protocol support. This protocol is also called the Panasonic protocol and is used by a number of manufacturers including Denon.
    
    The protocol includes a vendor field and a number of fields that are vendor specific. To support the format of address+command used by flipper the vendor+genre1+genre2+id fields are encoded into the address while the data is used for the command.
    
    There are older versions of the protocol that used a reverse bit order that are not supported.
    
    Protocol information:
    - https://github.com/Arduino-IRremote/Arduino-IRremote/blob/master/src/ir_Kaseikyo.hpp
    - http://www.hifi-remote.com/johnsfine/DecodeIR.html#Kaseikyo
    - https://www.denon.com/-/media/files/documentmaster/denonna/avr-x3700h_avc-x3700h_ir_code_v01_04062020.doc
    
    * Format and add unit test to Kaseikyo IR codec.
    
    Co-authored-by: Georgii Surkov <37121527+gsurkov@users.noreply.github.com>

---
+++ lib/infrared/encoder_decoder/kaseikyo/infrared_encoder_kaseikyo.c
@@ -0,0 +1,45 @@
+#include <core/check.h>
+#include "common/infrared_common_i.h"
+#include <stdint.h>
+#include "../infrared_i.h"
+#include "infrared_protocol_defs_i.h"
+#include <furi.h>
+
+void infrared_encoder_kaseikyo_reset(void* encoder_ptr, const InfraredMessage* message) {
+    furi_assert(encoder_ptr);
+
+    InfraredCommonEncoder* encoder = encoder_ptr;
+    infrared_common_encoder_reset(encoder);
+
+    uint32_t address = message->address;
+    uint16_t command = message->command;
+
+    uint8_t id = (address >> 24) & 3;
+    uint16_t vendor_id = (address >> 8) & 0xffff;
+    uint8_t genre1 = (address >> 4) & 0xf;
+    uint8_t genre2 = address & 0xf;
+
+    encoder->data[0] = (uint8_t)(vendor_id & 0xff);
+    encoder->data[1] = (uint8_t)(vendor_id >> 8);
+    uint8_t vendor_parity = encoder->data[0] ^ encoder->data[1];
+    vendor_parity = (vendor_parity & 0xf) ^ (vendor_parity >> 4);
+    encoder->data[2] = (vendor_parity & 0xf) | (genre1 << 4);
+    encoder->data[3] = (genre2 & 0xf) | ((uint8_t)(command & 0xf) << 4);
+    encoder->data[4] = (id << 6) | (uint8_t)(command >> 4);
+    encoder->data[5] = encoder->data[2] ^ encoder->data[3] ^ encoder->data[4];
+
+    encoder->bits_to_encode = encoder->protocol->databit_len[0];
+}
+
+void* infrared_encoder_kaseikyo_alloc(void) {
+    return infrared_common_encoder_alloc(&protocol_kaseikyo);
+}
+
+void infrared_encoder_kaseikyo_free(void* encoder_ptr) {
+    infrared_common_encoder_free(encoder_ptr);
+}
+
+InfraredStatus
+    infrared_encoder_kaseikyo_encode(void* encoder_ptr, uint32_t* duration, bool* level) {
+    return infrared_common_encode(encoder_ptr, duration, level);
+}
