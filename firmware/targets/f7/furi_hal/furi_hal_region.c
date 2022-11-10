b93f2dc2936e11a2d8de1a84ce1d7e06b962379a
MX <10697207+xMasterX@users.noreply.github.com>
2022-11-10 10:45:51 +0300

    Furi Region Mocks, fix protocol dict funcs was disabled in API

--- firmware/targets/f7/furi_hal/furi_hal_region.c
+++ firmware/targets/f7/furi_hal/furi_hal_region.c
@@ -1,4 +1,48 @@
 #include <furi_hal_region.h>
+#include <furi.h>
+
+const FuriHalRegion furi_hal_region_zero = {
+    .country_code = "00",
+    .bands_count = 1,
+    .bands = {
+        {
+            .start = 0,
+            .end = 1000000000,
+            .power_limit = 12,
+            .duty_cycle = 50,
+        },
+    }};
+
+static const FuriHalRegion* furi_hal_region = NULL;
+
+const FuriHalRegion* furi_hal_region_get() {
+    return &furi_hal_region_zero;
+}
+
+void furi_hal_region_set(FuriHalRegion* region) {
+    UNUSED(region);
+}
+
+const FuriHalRegionBand* furi_hal_region_get_band(uint32_t frequency) {
+    furi_hal_region = &furi_hal_region_zero;
+    if(!furi_hal_region) {
+        return NULL;
+    }
+
+    for(size_t i = 0; i < furi_hal_region->bands_count; i++) {
+        if(furi_hal_region->bands[i].start <= frequency &&
+           furi_hal_region->bands[i].end >= frequency) {
+            return &furi_hal_region->bands[i];
+        }
+    }
+
+    return NULL;
+}
+
+bool furi_hal_region_is_frequency_allowed(uint32_t frequency) {
+    UNUSED(frequency);
+    return true;
+}
 
 bool furi_hal_region_is_provisioned() {
     return true;
--- firmware/targets/furi_hal_include/furi_hal_region.h
+++ firmware/targets/furi_hal_include/furi_hal_region.h
@@ -2,10 +2,40 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <stddef.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef struct {
+    uint32_t start;
+    uint32_t end;
+    int8_t power_limit;
+    uint8_t duty_cycle;
+} FuriHalRegionBand;
+
+typedef struct {
+    char country_code[4];
+    uint16_t bands_count;
+    FuriHalRegionBand bands[];
+} FuriHalRegion;
+
+/** Get Region Data.
+ * 
+ * Region data may be allocated in Flash or in RAM.
+ * Keep in mind that we don't do memory management on our side.
+ *
+ * @return     pointer to FuriHalRegion instance (in RAM or Flash, check before freeing on region update)
+ */
+const FuriHalRegion* furi_hal_region_get();
+
+/** Set device region data
+ *
+ * @param      region  pointer to the FuriHalRegion
+ */
+void furi_hal_region_set(FuriHalRegion* region);
+
 /** Check if region data provisioned
  *
  * @return     true if provisioned, false otherwise
@@ -25,6 +55,25 @@ bool furi_hal_region_is_provisioned();
  * @return     Pointer to string
  */
 const char* furi_hal_region_get_name();
+
+/** Сheck if transmission is allowed on this frequency for your flipper region
+ *
+ * @param[in]  frequency  The frequency
+ * @param      value  frequency in Hz
+ *
+ * @return     true if allowed
+ */
+bool furi_hal_region_is_frequency_allowed(uint32_t frequency);
+
+/** Get band data for frequency
+ * 
+ * 
+ *
+ * @param[in]  frequency  The frequency
+ *
+ * @return     { description_of_the_return_value }
+ */
+const FuriHalRegionBand* furi_hal_region_get_band(uint32_t frequency);
 
 #ifdef __cplusplus
 }
