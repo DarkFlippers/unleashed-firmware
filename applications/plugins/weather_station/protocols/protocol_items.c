97e8da7a7bf781ef1670e38b52f984c551253d3f
MX <10697207+xMasterX@users.noreply.github.com>
2022-11-28 22:08:28 +0300

    Weather Station: Add protocol - Auriol HG0601A (#2056)
    * Add protocol Auriol hg0601a: Made by @LY2NEO
    * Improve readability of code: fix protocol name
    
    Co-authored-by: あく <alleteam@gmail.com>

--- applications/plugins/weather_station/protocols/protocol_items.c
+++ applications/plugins/weather_station/protocols/protocol_items.c
@@ -12,6 +12,7 @@ const SubGhzProtocol* weather_station_protocol_registry_items[] = {
     &ws_protocol_oregon2,
     &ws_protocol_acurite_592txr,
     &ws_protocol_ambient_weather,
+    &ws_protocol_auriol_th,
 };
 
 const SubGhzProtocolRegistry weather_station_protocol_registry = {
