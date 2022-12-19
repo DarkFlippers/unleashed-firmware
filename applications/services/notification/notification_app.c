94453d91003c38e7ebc946738056cbaefc414f2c
あく <alleteam@gmail.com>
2022-12-19 20:57:44 +0900

    [FL-3046] Notification: fix recursive speaker acquire #2147

--- applications/services/notification/notification_app.c
+++ applications/services/notification/notification_app.c
@@ -150,7 +150,7 @@ void notification_vibro_off() {
 }
 
 void notification_sound_on(float freq, float volume) {
-    if(furi_hal_speaker_acquire(30)) {
+    if(furi_hal_speaker_is_mine() || furi_hal_speaker_acquire(30)) {
         furi_hal_speaker_start(freq, volume);
     }
 }
