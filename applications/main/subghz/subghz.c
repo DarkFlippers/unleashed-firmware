27bfe861d98e5ed0d645de23a08dfb6470c85145
Dimitry <xstuff88@gmail.com>
2022-12-31 02:10:21 +0200

    cc1101 modem configurable preset introduced. Available to configure: datarate, bandwidth and manchester decoding flag
    log ability added to unit_test app (it's very helpful when writing a unit_tests)

--- applications/main/subghz/subghz.c
+++ applications/main/subghz/subghz.c
@@ -185,6 +185,12 @@ SubGhz* subghz_alloc(bool alloc_for_tx_only) {
 
     subghz_setting_load(subghz->setting, EXT_PATH("subghz/assets/setting_user"));
 
+    // Load custom advanced AM preset with configurable CFGMDM settings
+    FlipperFormat* advanced_am_preset = subghz_preset_custom_advanced_am_preset_alloc();
+    subghz_setting_load_custom_preset(
+        subghz->setting, ADVANCED_AM_PRESET_NAME, advanced_am_preset);
+    flipper_format_free(advanced_am_preset);
+
     // Custom Presets load without using config file
 
     FlipperFormat* temp_fm_preset = flipper_format_string_alloc();
