1d7966f74ef1b7ba04b536619b3b7d48804d3bd8
gornekich <n.gorbadey@gmail.com>
2023-06-01 16:37:47 +0400

    NFC: fix MFC timings (#2719)
    * digital signal: add optimization
    * nfc test: more restrict tests
    * digital signal: build as separate library
    * digital signal: remove unused flags, format sources
    * digital signal: fix cflag name
    * target: fix build for f18 target
    
    Co-authored-by: あく <alleteam@gmail.com>

--- lib/digital_signal/digital_signal.c
+++ lib/digital_signal/digital_signal.c
@@ -212,8 +212,7 @@ void digital_signal_prepare_arr(DigitalSignal* signal) {
     internals->reload_reg_entries = 0;
 
     for(size_t pos = 0; pos < signal->edge_cnt; pos++) {
-        uint32_t edge_scaled = (internals->factor * signal->edge_timings[pos]) / (1024 * 1024);
-        uint32_t pulse_duration = edge_scaled + internals->reload_reg_remainder;
+        uint32_t pulse_duration = signal->edge_timings[pos] + internals->reload_reg_remainder;
         if(pulse_duration < 10 || pulse_duration > 10000000) {
             FURI_LOG_D(
                 TAG,
