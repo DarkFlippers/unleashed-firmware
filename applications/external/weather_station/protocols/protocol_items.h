49d842e213f2a85be759bc4e5234281cbadf163f
clashlab <nicolabriet@gmail.com>
2023-06-09 14:18:32 +0200

    weather_station: add oregon3 with THGR221 (#2748)
    Co-authored-by: hedger <hedger@users.noreply.github.com>
    Co-authored-by: あく <alleteam@gmail.com>

--- applications/external/weather_station/protocols/protocol_items.h
+++ applications/external/weather_station/protocols/protocol_items.h
@@ -11,6 +11,7 @@
 #include "lacrosse_tx.h"
 #include "lacrosse_tx141thbv2.h"
 #include "oregon2.h"
+#include "oregon3.h"
 #include "acurite_592txr.h"
 #include "ambient_weather.h"
 #include "auriol_hg0601a.h"
