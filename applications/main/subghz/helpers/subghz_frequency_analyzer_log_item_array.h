0a5508a8a1ab202131262702901eaf0e388b212a
PpHd <patrick.pelissier@gmail.com>
2023-06-27 12:50:09 +0200

    Fix M*LIB usage (#2762)
    * Fix M*LIB usage
    * Fix oplist definition of SubGhzFrequencyAnalyzerLogItem
    * Fix oplist definition of M_CSTR_DUP_OPLIST
    * Remove dependency of furi_string_utf8_decode to the internal definition of string_unicode_t
    * Replace obsolete macro M_IF_DEFAULT1 to M_DEFAULT_ARGS
    
    Co-authored-by: hedger <hedger@users.noreply.github.com>
    Co-authored-by: あく <alleteam@gmail.com>

--- applications/main/subghz/helpers/subghz_frequency_analyzer_log_item_array.h
+++ applications/main/subghz/helpers/subghz_frequency_analyzer_log_item_array.h
@@ -27,7 +27,12 @@ TUPLE_DEF2(
     (rssi_max, uint8_t))
 /* Register globally the oplist */
 #define M_OPL_SubGhzFrequencyAnalyzerLogItem_t() \
-    TUPLE_OPLIST(SubGhzFrequencyAnalyzerLogItem, M_POD_OPLIST, M_DEFAULT_OPLIST, M_DEFAULT_OPLIST)
+    TUPLE_OPLIST(                                \
+        SubGhzFrequencyAnalyzerLogItem,          \
+        M_DEFAULT_OPLIST,                        \
+        M_DEFAULT_OPLIST,                        \
+        M_DEFAULT_OPLIST,                        \
+        M_DEFAULT_OPLIST)
 
 /* Define the array, register the oplist and define further algorithms on it */
 ARRAY_DEF(SubGhzFrequencyAnalyzerLogItemArray, SubGhzFrequencyAnalyzerLogItem_t)
