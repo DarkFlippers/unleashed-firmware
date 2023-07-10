0e1a54141ee83b56dd065fe979a6550e9ff8927c
gid9798 <30450294+gid9798@users.noreply.github.com>
2023-07-10 19:59:25 +0300

    Fix SubGhz Apps & LF RFID

--- firmware/targets/f7/platform_specific/intrinsic_export.h
+++ firmware/targets/f7/platform_specific/intrinsic_export.h
@@ -6,6 +6,7 @@ extern "C" {
 #endif
 
 void __clear_cache(void*, void*);
+void* __aeabi_uldivmod(uint64_t, uint64_t);
 
 #ifdef __cplusplus
 }
--- lib/subghz/subghz_protocol_registry.h
+++ lib/subghz/subghz_protocol_registry.h
@@ -27,11 +27,63 @@ bool subghz_protocol_keeloq_create_data(
     const char* manufacture_name,
     SubGhzRadioPreset* preset);
 
+bool subghz_protocol_keeloq_bft_create_data(
+    void* context,
+    FlipperFormat* flipper_format,
+    uint32_t serial,
+    uint8_t btn,
+    uint16_t cnt,
+    uint32_t seed,
+    const char* manufacture_name,
+    SubGhzRadioPreset* preset);
+
 void subghz_protocol_decoder_bin_raw_data_input_rssi(
     SubGhzProtocolDecoderBinRAW* instance,
     float rssi);
 
 bool subghz_protocol_secplus_v1_check_fixed(uint32_t fixed);
+
+bool subghz_protocol_faac_slh_create_data(
+    void* context,
+    FlipperFormat* flipper_format,
+    uint32_t serial,
+    uint8_t btn,
+    uint32_t cnt,
+    uint32_t seed,
+    const char* manufacture_name,
+    SubGhzRadioPreset* preset);
+
+bool subghz_protocol_came_atomo_create_data(
+    void* context,
+    FlipperFormat* flipper_format,
+    uint32_t serial,
+    uint16_t cnt,
+    SubGhzRadioPreset* preset);
+
+bool subghz_protocol_somfy_telis_create_data(
+    void* context,
+    FlipperFormat* flipper_format,
+    uint32_t serial,
+    uint8_t btn,
+    uint16_t cnt,
+    SubGhzRadioPreset* preset);
+
+bool subghz_protocol_nice_flor_s_create_data(
+    void* context,
+    FlipperFormat* flipper_format,
+    uint32_t serial,
+    uint8_t btn,
+    uint16_t cnt,
+    SubGhzRadioPreset* preset,
+    bool nice_one);
+
+bool subghz_protocol_alutech_at_4n_create_data(
+    void* context,
+    FlipperFormat* flipper_format,
+    uint32_t serial,
+    uint8_t btn,
+    uint16_t cnt,
+    SubGhzRadioPreset* preset);
 
 #ifdef __cplusplus
 }
