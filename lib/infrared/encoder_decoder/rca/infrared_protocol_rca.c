14fc960246a4dccb0a17cdc5fe1254ba5180de29
MMX <10697207+xMasterX@users.noreply.github.com>
2023-07-11 11:39:07 +0300

    Infrared: RCA protocol support (#2823)
    * RCA protocol support
    * Add unit test
    
    Co-authored-by: あく <alleteam@gmail.com>

---
+++ lib/infrared/encoder_decoder/rca/infrared_protocol_rca.c
@@ -0,0 +1,40 @@
+#include "infrared_protocol_rca_i.h"
+
+const InfraredCommonProtocolSpec infrared_protocol_rca = {
+    .timings =
+        {
+            .preamble_mark = INFRARED_RCA_PREAMBLE_MARK,
+            .preamble_space = INFRARED_RCA_PREAMBLE_SPACE,
+            .bit1_mark = INFRARED_RCA_BIT1_MARK,
+            .bit1_space = INFRARED_RCA_BIT1_SPACE,
+            .bit0_mark = INFRARED_RCA_BIT0_MARK,
+            .bit0_space = INFRARED_RCA_BIT0_SPACE,
+            .preamble_tolerance = INFRARED_RCA_PREAMBLE_TOLERANCE,
+            .bit_tolerance = INFRARED_RCA_BIT_TOLERANCE,
+            .silence_time = INFRARED_RCA_SILENCE,
+            .min_split_time = INFRARED_RCA_MIN_SPLIT_TIME,
+        },
+    .databit_len[0] = 24,
+    .no_stop_bit = false,
+    .decode = infrared_common_decode_pdwm,
+    .encode = infrared_common_encode_pdwm,
+    .interpret = infrared_decoder_rca_interpret,
+    .decode_repeat = NULL,
+    .encode_repeat = NULL,
+};
+
+static const InfraredProtocolVariant infrared_protocol_variant_rca = {
+    .name = "RCA",
+    .address_length = 4,
+    .command_length = 8,
+    .frequency = INFRARED_COMMON_CARRIER_FREQUENCY,
+    .duty_cycle = INFRARED_COMMON_DUTY_CYCLE,
+    .repeat_count = INFRARED_RCA_REPEAT_COUNT_MIN,
+};
+
+const InfraredProtocolVariant* infrared_protocol_rca_get_variant(InfraredProtocol protocol) {
+    if(protocol == InfraredProtocolRCA)
+        return &infrared_protocol_variant_rca;
+    else
+        return NULL;
+}
---
+++ lib/infrared/encoder_decoder/rca/infrared_protocol_rca_i.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../common/infrared_common_i.h"
+
+#define INFRARED_RCA_PREAMBLE_MARK 4000
+#define INFRARED_RCA_PREAMBLE_SPACE 4000
+#define INFRARED_RCA_BIT1_MARK 500
+#define INFRARED_RCA_BIT1_SPACE 2000
+#define INFRARED_RCA_BIT0_MARK 500
+#define INFRARED_RCA_BIT0_SPACE 1000
+#define INFRARED_RCA_REPEAT_PERIOD 8000
+#define INFRARED_RCA_SILENCE INFRARED_RCA_REPEAT_PERIOD
+
+#define INFRARED_RCA_MIN_SPLIT_TIME INFRARED_RCA_REPEAT_PAUSE_MIN
+#define INFRARED_RCA_REPEAT_PAUSE_MIN 4000
+#define INFRARED_RCA_REPEAT_PAUSE_MAX 150000
+#define INFRARED_RCA_REPEAT_COUNT_MIN 1
+#define INFRARED_RCA_REPEAT_MARK INFRARED_RCA_PREAMBLE_MARK
+#define INFRARED_RCA_REPEAT_SPACE INFRARED_RCA_PREAMBLE_SPACE
+#define INFRARED_RCA_PREAMBLE_TOLERANCE 200 // us
+#define INFRARED_RCA_BIT_TOLERANCE 120 // us
+
+extern const InfraredCommonProtocolSpec infrared_protocol_rca;
+
+bool infrared_decoder_rca_interpret(InfraredCommonDecoder* decoder);
+InfraredStatus infrared_decoder_rca_decode_repeat(InfraredCommonDecoder* decoder);
+InfraredStatus infrared_encoder_rca_encode_repeat(
+    InfraredCommonEncoder* encoder,
+    uint32_t* duration,
+    bool* level);
