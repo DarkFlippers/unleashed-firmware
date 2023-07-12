61a66c52c61ea0fb862c0a564fd918326952b4cd
MX <10697207+xMasterX@users.noreply.github.com>
2023-07-12 21:23:49 +0300

    Revert "merge changes"
    This reverts commit f998948623d8cb8bb1ead7efe839682f96f34d3c.

--- applications/services/gui/modules/file_browser.c
+++ applications/services/gui/modules/file_browser.c
@@ -480,33 +480,25 @@ static void browser_list_item_cb(
             browser->view,
             FileBrowserModel * model,
             {
-                model->list_loading = false;
-                if(browser_is_list_load_required(model)) {
-                    model->list_loading = true;
-                    int32_t load_offset = CLAMP(
-                        model->item_idx - ITEM_LIST_LEN_MAX / 2, (int32_t)model->item_cnt, 0);
-                    file_browser_worker_load(browser->worker, load_offset, ITEM_LIST_LEN_MAX);
+                if(model->item_cnt <= BROWSER_SORT_THRESHOLD) {
+                    FuriString* selected = NULL;
+                    if(model->item_idx > 0) {
+                        selected = furi_string_alloc_set(
+                            items_array_get(model->items, model->item_idx)->path);
+                    }
 
-                    if(model->item_cnt <= BROWSER_SORT_THRESHOLD) {
-                        FuriString* selected = NULL;
-                        if(model->item_idx > 0) {
-                            selected = furi_string_alloc_set(
-                                items_array_get(model->items, model->item_idx)->path);
-                        }
+                    items_array_sort(model->items);
 
-                        items_array_sort(model->items);
-
-                        if(selected != NULL) {
-                            for(uint32_t i = 0; i < model->item_cnt; i++) {
-                                if(!furi_string_cmp(
-                                       items_array_get(model->items, i)->path, selected)) {
-                                    model->item_idx = i;
-                                    break;
-                                }
+                    if(selected != NULL) {
+                        for(uint32_t i = 0; i < model->item_cnt; i++) {
+                            if(!furi_string_cmp(items_array_get(model->items, i)->path, selected)) {
+                                model->item_idx = i;
+                                break;
                             }
                         }
                     }
                 }
+                model->list_loading = false;
             },
             false);
         browser_update_offset(browser);
