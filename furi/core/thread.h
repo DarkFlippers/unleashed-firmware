c72531edc636ddbacda75d86f9bb74e0f1bb92fb
Leopold <leommxj@gmail.com>
2023-08-02 12:23:04 +0800

    change FuriThreadPriorityIsr to 31 (configMAX_PRIORITIES-1) (#2920)
    * change FuriThreadPriorityIsr to 31 (configMAX_PRIORITIES-1)
    * Furi: less hardcoded max priority, fix spelling
    * Format sources
    
    Co-authored-by: hedger <hedger@users.noreply.github.com>
    Co-authored-by: あく <alleteam@gmail.com>

--- furi/core/thread.h
+++ furi/core/thread.h
@@ -28,7 +28,7 @@ typedef enum {
     FuriThreadPriorityNormal = 16, /**< Normal */
     FuriThreadPriorityHigh = 17, /**< High */
     FuriThreadPriorityHighest = 18, /**< Highest */
-    FuriThreadPriorityIsr = 32, /**< Deffered Isr (highest possible) */
+    FuriThreadPriorityIsr = (configMAX_PRIORITIES - 1), /**< Deferred ISR (highest possible) */
 } FuriThreadPriority;
 
 /** FuriThread anonymous structure */
