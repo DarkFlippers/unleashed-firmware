8d8102b4f9f63d8f6e063d809087d465f4a752bd
MX <10697207+xMasterX@users.noreply.github.com>
2023-08-11 18:24:51 +0300

    merge fixes

--- applications/settings/desktop_settings/scenes/desktop_settings_scene_start.c
+++ applications/settings/desktop_settings/scenes/desktop_settings_scene_start.c
@@ -167,12 +167,7 @@ bool desktop_settings_scene_start_on_event(void* context, SceneManagerEvent even
             break;
         case SCENE_EVENT_SELECT_AUTO_LOCK_DELAY:
         case SCENE_EVENT_SELECT_CLOCK_DISPLAY:
-            consumed = true;
-            break;
         case SCENE_EVENT_SELECT_BATTERY_DISPLAY:
-            consumed = true;
-            break;
-        case SCENE_EVENT_SELECT_CLOCK_DISPLAY:
             consumed = true;
             break;
         case SCENE_EVENT_SELECT_CHANGE_NAME:
