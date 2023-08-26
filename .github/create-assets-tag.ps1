a83b3287851c58be8b0aa0f00dd941b9a369264a
DerSkythe <skif@skif.ws>
2023-08-26 22:56:25 +0400

    change: debug derskythe-fw-build.yml

--- .github/create-assets-tag.ps1
+++ .github/create-assets-tag.ps1
@@ -3,48 +3,64 @@ Set-StrictMode -Version 3.0            #
 $ErrorActionPreference = "Stop"        #
 ########################################
 
-function Format-Bytes {
+function Format-Bytes
+{
     param(
         [int]$number
     )
     [string]$sizes = 'KB', 'MB', 'GB', 'TB', 'PB'
     [int64]$num = 0
     for ($x = 0; $x -lt $sizes.count; $x++) {
-        if ($number -lt [int64]"1$($sizes[$x])") {
-            if ($x -eq 0) {
+        if ($number -lt [int64]"1$( $sizes[$x] )")
+        {
+            if ($x -eq 0)
+            {
                 return "$number B"
             }
-            else {
-                $num = $number / [int64]"1$($sizes[$x-1])"
+            else
+            {
+                $num = $number / [int64]"1$( $sizes[$x - 1] )"
                 $num = "{0:N2}" -f $num
-                return "$num $($sizes[$x-1])"
+                return "$num $( $sizes[$x - 1] )"
             }
         }
     }
 }
 
-[string]$ZipName = $args[0]
-[string]$TgzName = $args[1]
-[string]$AppDir = $args[2]
-[int32]$ForGithubActions = $args[3]
+[string]$ExtraTgzName = $args[0]
+[string]$DefaultZipName = $args[1]
+[string]$DefaultTgzName = $args[2]
+[string]$AppDir = $args[3]
+[int32]$ForGithubActions = $args[4]
 
 $Output = @{
-    ZIP_NAME = $ZipName
-    TGZ_NAME = $TgzName
-    ZIP_TAG  = ''
-    TGZ_TAG  = ''
+    TGZ_NAME_EXTRA_APPS = $ExtraTgzName
+    TGZ_NAME_DEFAULT_APPS = $DefaultTgzName
+    ZIP_NAME_DEFAULT_APPS = $DefaultZipName
+    TGZ_TAG_EXTRA_APPS = ''
+    TGZ_TAG_DEFAULT_APPS = ''
+    ZIP_TAG_DEFAULT_APPS = ''
 }
 
-$Size = (Get-Item -Path "$AppDir/$ZipName" | Get-ItemPropertyValue -Name Length)
+if (!(Test-Path -Path "$AppDir/$DefaultZipName" -PathType Leaf) -or !(Test-Path -Path "$AppDir/$ExtraTgzName" -PathType Leaf) -or !(Test-Path -Path "$AppDir/$DefaultTgzName" -PathType Leaf))
+{
+    Write-Error '::error title=Files not found::Cannot find files in location'
+    exit 1
+}
+
+$Size = (Get-Item -Path "$AppDir/$DefaultZipName" | Get-ItemPropertyValue -Name Length)
 Write-Output ('Filesize: {0}' -f (Format-Bytes $Size))
 
-$ZipSize = Format-Bytes (Get-Item -Path "$AppDir/$ZipName").Length
-$TgzSize = Format-Bytes (Get-Item -Path "$AppDir/$TgzName" ).Length
+$DefaultZipSize = Format-Bytes (Get-Item -Path "$AppDir/$DefaultZipName").Length
+$DefaultTgzSize = Format-Bytes (Get-Item -Path "$AppDir/$DefaultTgzName").Length
+$ExtraTgzSize = Format-Bytes (Get-Item -Path "$AppDir/$ExtraTgzName").Length
 
-$Output.ZIP_TAG = '{0} ({1})' -f $ZipName, $ZipSize
-$Output.TGZ_TAG = '{0} ({1})' -f $TgzName, $TgzSize
+$Output.ZIP_TAG_DEFAULT_APPS = '{0} ({1})' -f $DefaultZipName, $DefaultZipSize
+$Output.TGZ_TAG_DEFAULT_APPS = '{0} ({1})' -f $DefaultTgzName, $DefaultTgzSize
+$Output.TGZ_TAG_EXTRA_APPS = '{0} ({1})' -f $ExtraTgzName, $ExtraTgzSize
 
-if ($ForGithubActions) {
+if ($ForGithubActions)
+{
     $Plain = New-Object -TypeName "System.Text.StringBuilder"
     $Output.GetEnumerator() | ForEach-Object {
         [void]$Plain.Append($_.Key)
@@ -53,6 +69,7 @@ if ($ForGithubActions) {
     }
     Write-Output $Plain.ToString()
 }
-else {
+else
+{
     $Output
 }
