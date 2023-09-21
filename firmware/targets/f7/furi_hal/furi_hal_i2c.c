f46018b2046edc7e49546db4c5b7f375c828ab46
agarof <18119032+agarof@users.noreply.github.com>
2023-09-21 09:54:25 +0200

    Add extended I2C HAL functions (#3037)
    * Add extended I2C HAL functions
    * Rename I2CEndClockStretch to I2CEndAwaitRestart
    * Address review comments
    * Update f18 api_symbols.csv
    * FuriHal: check input values in cortex timer
    * FuriHal: cleanup I2C documentation
    * Properly bump api symbols
    * FuriHal: fix incorrect cast in I2C write_reg methods, fix spelling and naming
    * FuriHal: cleanup const usage in I2C, sync declaration and implementation
    * Format Sources
    * FuriHal: more i2c docs
    * Add I2C Restart and Pause / Resume test
    * Add I2C auto-reload test
    * UnitTests: skip furi_hal_i2c_ext_eeprom if eeprom is not connected
    * UnitTest: cleanup subghz test output
    * FuriHal: classic timeouts in i2c
    
    Co-authored-by: hedger <hedger@users.noreply.github.com>
    Co-authored-by: あく <alleteam@gmail.com>

--- firmware/targets/f7/furi_hal/furi_hal_i2c.c
+++ firmware/targets/f7/furi_hal/furi_hal_i2c.c
@@ -5,7 +5,6 @@
 
 #include <stm32wbxx_ll_i2c.h>
 #include <stm32wbxx_ll_gpio.h>
-#include <stm32wbxx_ll_cortex.h>
 #include <furi.h>
 
 #define TAG "FuriHalI2c"
@@ -27,7 +26,7 @@ void furi_hal_i2c_acquire(FuriHalI2cBusHandle* handle) {
     furi_hal_power_insomnia_enter();
     // Lock bus access
     handle->bus->callback(handle->bus, FuriHalI2cBusEventLock);
-    // Ensuree that no active handle set
+    // Ensure that no active handle set
     furi_check(handle->bus->current_handle == NULL);
     // Set current handle
     handle->bus->current_handle = handle;
@@ -51,177 +50,265 @@ void furi_hal_i2c_release(FuriHalI2cBusHandle* handle) {
     furi_hal_power_insomnia_exit();
 }
 
+static bool
+    furi_hal_i2c_wait_for_idle(I2C_TypeDef* i2c, FuriHalI2cBegin begin, FuriHalCortexTimer timer) {
+    do {
+        if(furi_hal_cortex_timer_is_expired(timer)) {
+            return false;
+        }
+    } while(begin == FuriHalI2cBeginStart && LL_I2C_IsActiveFlag_BUSY(i2c));
+    // Only check if the bus is busy if starting a new transaction, if not we already control the bus
+
+    return true;
+}
+
+static bool
+    furi_hal_i2c_wait_for_end(I2C_TypeDef* i2c, FuriHalI2cEnd end, FuriHalCortexTimer timer) {
+    // If ending the transaction with a stop condition, wait for it to be detected, otherwise wait for a transfer complete flag
+    bool wait_for_stop = end == FuriHalI2cEndStop;
+    uint32_t end_mask = (wait_for_stop) ? I2C_ISR_STOPF : (I2C_ISR_TC | I2C_ISR_TCR);
+
+    while((i2c->ISR & end_mask) == 0) {
+        if(furi_hal_cortex_timer_is_expired(timer)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+static uint32_t
+    furi_hal_i2c_get_start_signal(FuriHalI2cBegin begin, bool ten_bit_address, bool read) {
+    switch(begin) {
+    case FuriHalI2cBeginRestart:
+        if(read) {
+            return ten_bit_address ? LL_I2C_GENERATE_RESTART_10BIT_READ :
+                                     LL_I2C_GENERATE_RESTART_7BIT_READ;
+        } else {
+            return ten_bit_address ? LL_I2C_GENERATE_RESTART_10BIT_WRITE :
+                                     LL_I2C_GENERATE_RESTART_7BIT_WRITE;
+        }
+    case FuriHalI2cBeginResume:
+        return LL_I2C_GENERATE_NOSTARTSTOP;
+    case FuriHalI2cBeginStart:
+    default:
+        return read ? LL_I2C_GENERATE_START_READ : LL_I2C_GENERATE_START_WRITE;
+    }
+}
+
+static uint32_t furi_hal_i2c_get_end_signal(FuriHalI2cEnd end) {
+    switch(end) {
+    case FuriHalI2cEndAwaitRestart:
+        return LL_I2C_MODE_SOFTEND;
+    case FuriHalI2cEndPause:
+        return LL_I2C_MODE_RELOAD;
+    case FuriHalI2cEndStop:
+    default:
+        return LL_I2C_MODE_AUTOEND;
+    }
+}
+
+static bool furi_hal_i2c_transfer_is_aborted(I2C_TypeDef* i2c) {
+    return LL_I2C_IsActiveFlag_STOP(i2c) &&
+           !(LL_I2C_IsActiveFlag_TC(i2c) || LL_I2C_IsActiveFlag_TCR(i2c));
+}
+
+static bool furi_hal_i2c_transfer(
+    I2C_TypeDef* i2c,
+    uint8_t* data,
+    uint32_t size,
+    FuriHalI2cEnd end,
+    bool read,
+    FuriHalCortexTimer timer) {
+    bool ret = true;
+
+    while(size > 0) {
+        bool should_stop = furi_hal_cortex_timer_is_expired(timer) ||
+                           furi_hal_i2c_transfer_is_aborted(i2c);
+
+        // Modifying the data pointer's data is UB if read is true
+        if(read && LL_I2C_IsActiveFlag_RXNE(i2c)) {
+            *data = LL_I2C_ReceiveData8(i2c);
+            data++;
+            size--;
+        } else if(!read && LL_I2C_IsActiveFlag_TXIS(i2c)) {
+            LL_I2C_TransmitData8(i2c, *data);
+            data++;
+            size--;
+        }
+
+        // Exit on timeout or premature stop, probably caused by a nacked address or byte
+        if(should_stop) {
+            ret = size == 0; // If the transfer was over, still a success
+            break;
+        }
+    }
+
+    if(ret) {
+        ret = furi_hal_i2c_wait_for_end(i2c, end, timer);
+    }
+
+    LL_I2C_ClearFlag_STOP(i2c);
+
+    return ret;
+}
+
+static bool furi_hal_i2c_transaction(
+    I2C_TypeDef* i2c,
+    uint16_t address,
+    bool ten_bit,
+    uint8_t* data,
+    size_t size,
+    FuriHalI2cBegin begin,
+    FuriHalI2cEnd end,
+    bool read,
+    FuriHalCortexTimer timer) {
+    uint32_t addr_size = ten_bit ? LL_I2C_ADDRSLAVE_10BIT : LL_I2C_ADDRSLAVE_7BIT;
+    uint32_t start_signal = furi_hal_i2c_get_start_signal(begin, ten_bit, read);
+
+    if(!furi_hal_i2c_wait_for_idle(i2c, begin, timer)) {
+        return false;
+    }
+
+    do {
+        uint8_t transfer_size = size;
+        FuriHalI2cEnd transfer_end = end;
+
+        if(size > 255) {
+            transfer_size = 255;
+            transfer_end = FuriHalI2cEndPause;
+        }
+
+        uint32_t end_signal = furi_hal_i2c_get_end_signal(transfer_end);
+
+        LL_I2C_HandleTransfer(i2c, address, addr_size, transfer_size, end_signal, start_signal);
+
+        if(!furi_hal_i2c_transfer(i2c, data, transfer_size, transfer_end, read, timer)) {
+            return false;
+        }
+
+        size -= transfer_size;
+        data += transfer_size;
+        start_signal = LL_I2C_GENERATE_NOSTARTSTOP;
+    } while(size > 0);
+
+    return true;
+}
+
+bool furi_hal_i2c_rx_ext(
+    FuriHalI2cBusHandle* handle,
+    uint16_t address,
+    bool ten_bit,
+    uint8_t* data,
+    size_t size,
+    FuriHalI2cBegin begin,
+    FuriHalI2cEnd end,
+    uint32_t timeout) {
+    furi_check(handle->bus->current_handle == handle);
+
+    FuriHalCortexTimer timer = furi_hal_cortex_timer_get(timeout * 1000);
+
+    return furi_hal_i2c_transaction(
+        handle->bus->i2c, address, ten_bit, data, size, begin, end, true, timer);
+}
+
+bool furi_hal_i2c_tx_ext(
+    FuriHalI2cBusHandle* handle,
+    uint16_t address,
+    bool ten_bit,
+    const uint8_t* data,
+    size_t size,
+    FuriHalI2cBegin begin,
+    FuriHalI2cEnd end,
+    uint32_t timeout) {
+    furi_check(handle->bus->current_handle == handle);
+
+    FuriHalCortexTimer timer = furi_hal_cortex_timer_get(timeout * 1000);
+
+    return furi_hal_i2c_transaction(
+        handle->bus->i2c, address, ten_bit, (uint8_t*)data, size, begin, end, false, timer);
+}
+
 bool furi_hal_i2c_tx(
     FuriHalI2cBusHandle* handle,
     uint8_t address,
     const uint8_t* data,
-    uint8_t size,
+    size_t size,
     uint32_t timeout) {
-    furi_check(handle->bus->current_handle == handle);
     furi_assert(timeout > 0);
 
-    bool ret = true;
-    FuriHalCortexTimer timer = furi_hal_cortex_timer_get(timeout * 1000);
-
-    do {
-        while(LL_I2C_IsActiveFlag_BUSY(handle->bus->i2c)) {
-            if(furi_hal_cortex_timer_is_expired(timer)) {
-                ret = false;
-                break;
-            }
-        }
-
-        if(!ret) {
-            break;
-        }
-
-        LL_I2C_HandleTransfer(
-            handle->bus->i2c,
-            address,
-            LL_I2C_ADDRSLAVE_7BIT,
-            size,
-            LL_I2C_MODE_AUTOEND,
-            LL_I2C_GENERATE_START_WRITE);
-
-        while(!LL_I2C_IsActiveFlag_STOP(handle->bus->i2c) || size > 0) {
-            if(LL_I2C_IsActiveFlag_TXIS(handle->bus->i2c)) {
-                LL_I2C_TransmitData8(handle->bus->i2c, (*data));
-                data++;
-                size--;
-            }
-
-            if(furi_hal_cortex_timer_is_expired(timer)) {
-                ret = false;
-                break;
-            }
-        }
-
-        LL_I2C_ClearFlag_STOP(handle->bus->i2c);
-    } while(0);
-
-    return ret;
+    return furi_hal_i2c_tx_ext(
+        handle, address, false, data, size, FuriHalI2cBeginStart, FuriHalI2cEndStop, timeout);
 }
 
 bool furi_hal_i2c_rx(
     FuriHalI2cBusHandle* handle,
     uint8_t address,
     uint8_t* data,
-    uint8_t size,
+    size_t size,
     uint32_t timeout) {
-    furi_check(handle->bus->current_handle == handle);
     furi_assert(timeout > 0);
 
-    bool ret = true;
-    FuriHalCortexTimer timer = furi_hal_cortex_timer_get(timeout * 1000);
-
-    do {
-        while(LL_I2C_IsActiveFlag_BUSY(handle->bus->i2c)) {
-            if(furi_hal_cortex_timer_is_expired(timer)) {
-                ret = false;
-                break;
-            }
-        }
-
-        if(!ret) {
-            break;
-        }
-
-        LL_I2C_HandleTransfer(
-            handle->bus->i2c,
-            address,
-            LL_I2C_ADDRSLAVE_7BIT,
-            size,
-            LL_I2C_MODE_AUTOEND,
-            LL_I2C_GENERATE_START_READ);
-
-        while(!LL_I2C_IsActiveFlag_STOP(handle->bus->i2c) || size > 0) {
-            if(LL_I2C_IsActiveFlag_RXNE(handle->bus->i2c)) {
-                *data = LL_I2C_ReceiveData8(handle->bus->i2c);
-                data++;
-                size--;
-            }
-
-            if(furi_hal_cortex_timer_is_expired(timer)) {
-                ret = false;
-                break;
-            }
-        }
-
-        LL_I2C_ClearFlag_STOP(handle->bus->i2c);
-    } while(0);
-
-    return ret;
+    return furi_hal_i2c_rx_ext(
+        handle, address, false, data, size, FuriHalI2cBeginStart, FuriHalI2cEndStop, timeout);
 }
 
 bool furi_hal_i2c_trx(
     FuriHalI2cBusHandle* handle,
     uint8_t address,
     const uint8_t* tx_data,
-    uint8_t tx_size,
+    size_t tx_size,
     uint8_t* rx_data,
-    uint8_t rx_size,
+    size_t rx_size,
     uint32_t timeout) {
-    if(furi_hal_i2c_tx(handle, address, tx_data, tx_size, timeout) &&
-       furi_hal_i2c_rx(handle, address, rx_data, rx_size, timeout)) {
-        return true;
-    } else {
-        return false;
-    }
+    return furi_hal_i2c_tx_ext(
+               handle,
+               address,
+               false,
+               tx_data,
+               tx_size,
+               FuriHalI2cBeginStart,
+               FuriHalI2cEndStop,
+               timeout) &&
+           furi_hal_i2c_rx_ext(
+               handle,
+               address,
+               false,
+               rx_data,
+               rx_size,
+               FuriHalI2cBeginStart,
+               FuriHalI2cEndStop,
+               timeout);
 }
 
 bool furi_hal_i2c_is_device_ready(FuriHalI2cBusHandle* handle, uint8_t i2c_addr, uint32_t timeout) {
     furi_check(handle);
-
     furi_check(handle->bus->current_handle == handle);
     furi_assert(timeout > 0);
 
     bool ret = true;
     FuriHalCortexTimer timer = furi_hal_cortex_timer_get(timeout * 1000);
 
-    do {
-        while(LL_I2C_IsActiveFlag_BUSY(handle->bus->i2c)) {
-            if(furi_hal_cortex_timer_is_expired(timer)) {
-                return false;
-            }
-        }
+    if(!furi_hal_i2c_wait_for_idle(handle->bus->i2c, FuriHalI2cBeginStart, timer)) {
+        return false;
+    }
 
-        handle->bus->i2c->CR2 =
-            ((((uint32_t)(i2c_addr) & (I2C_CR2_SADD)) | (I2C_CR2_START) | (I2C_CR2_AUTOEND)) &
-             (~I2C_CR2_RD_WRN));
+    LL_I2C_HandleTransfer(
+        handle->bus->i2c,
+        i2c_addr,
+        LL_I2C_ADDRSLAVE_7BIT,
+        0,
+        LL_I2C_MODE_AUTOEND,
+        LL_I2C_GENERATE_START_WRITE);
 
-        while((!LL_I2C_IsActiveFlag_NACK(handle->bus->i2c)) &&
-              (!LL_I2C_IsActiveFlag_STOP(handle->bus->i2c))) {
-            if(furi_hal_cortex_timer_is_expired(timer)) {
-                return false;
-            }
-        }
+    if(!furi_hal_i2c_wait_for_end(handle->bus->i2c, FuriHalI2cEndStop, timer)) {
+        return false;
+    }
 
-        if(LL_I2C_IsActiveFlag_NACK(handle->bus->i2c)) {
-            while(!LL_I2C_IsActiveFlag_STOP(handle->bus->i2c)) {
-                if(furi_hal_cortex_timer_is_expired(timer)) {
-                    return false;
-                }
-            }
+    ret = !LL_I2C_IsActiveFlag_NACK(handle->bus->i2c);
 
-            LL_I2C_ClearFlag_NACK(handle->bus->i2c);
-
-            // Clear STOP Flag generated by autoend
-            LL_I2C_ClearFlag_STOP(handle->bus->i2c);
-
-            // Generate actual STOP
-            LL_I2C_GenerateStopCondition(handle->bus->i2c);
-
-            ret = false;
-        }
-
-        while(!LL_I2C_IsActiveFlag_STOP(handle->bus->i2c)) {
-            if(furi_hal_cortex_timer_is_expired(timer)) {
-                return false;
-            }
-        }
-
-        LL_I2C_ClearFlag_STOP(handle->bus->i2c);
-    } while(0);
+    LL_I2C_ClearFlag_NACK(handle->bus->i2c);
+    LL_I2C_ClearFlag_STOP(handle->bus->i2c);
 
     return ret;
 }
@@ -257,7 +344,7 @@ bool furi_hal_i2c_read_mem(
     uint8_t i2c_addr,
     uint8_t mem_addr,
     uint8_t* data,
-    uint8_t len,
+    size_t len,
     uint32_t timeout) {
     furi_check(handle);
 
@@ -272,11 +359,12 @@ bool furi_hal_i2c_write_reg_8(
     uint32_t timeout) {
     furi_check(handle);
 
-    uint8_t tx_data[2];
-    tx_data[0] = reg_addr;
-    tx_data[1] = data;
+    const uint8_t tx_data[2] = {
+        reg_addr,
+        data,
+    };
 
-    return furi_hal_i2c_tx(handle, i2c_addr, (const uint8_t*)&tx_data, 2, timeout);
+    return furi_hal_i2c_tx(handle, i2c_addr, tx_data, 2, timeout);
 }
 
 bool furi_hal_i2c_write_reg_16(
@@ -287,69 +375,42 @@ bool furi_hal_i2c_write_reg_16(
     uint32_t timeout) {
     furi_check(handle);
 
-    uint8_t tx_data[3];
-    tx_data[0] = reg_addr;
-    tx_data[1] = (data >> 8) & 0xFF;
-    tx_data[2] = data & 0xFF;
+    const uint8_t tx_data[3] = {
+        reg_addr,
+        (data >> 8) & 0xFF,
+        data & 0xFF,
+    };
 
-    return furi_hal_i2c_tx(handle, i2c_addr, (const uint8_t*)&tx_data, 3, timeout);
+    return furi_hal_i2c_tx(handle, i2c_addr, tx_data, 3, timeout);
 }
 
 bool furi_hal_i2c_write_mem(
     FuriHalI2cBusHandle* handle,
     uint8_t i2c_addr,
     uint8_t mem_addr,
-    uint8_t* data,
-    uint8_t len,
+    const uint8_t* data,
+    size_t len,
     uint32_t timeout) {
     furi_check(handle);
-
     furi_check(handle->bus->current_handle == handle);
     furi_assert(timeout > 0);
 
-    bool ret = true;
-    uint8_t size = len + 1;
-    FuriHalCortexTimer timer = furi_hal_cortex_timer_get(timeout * 1000);
-
-    do {
-        while(LL_I2C_IsActiveFlag_BUSY(handle->bus->i2c)) {
-            if(furi_hal_cortex_timer_is_expired(timer)) {
-                ret = false;
-                break;
-            }
-        }
-
-        if(!ret) {
-            break;
-        }
-
-        LL_I2C_HandleTransfer(
-            handle->bus->i2c,
-            i2c_addr,
-            LL_I2C_ADDRSLAVE_7BIT,
-            size,
-            LL_I2C_MODE_AUTOEND,
-            LL_I2C_GENERATE_START_WRITE);
-
-        while(!LL_I2C_IsActiveFlag_STOP(handle->bus->i2c) || size > 0) {
-            if(LL_I2C_IsActiveFlag_TXIS(handle->bus->i2c)) {
-                if(size == len + 1) {
-                    LL_I2C_TransmitData8(handle->bus->i2c, mem_addr);
-                } else {
-                    LL_I2C_TransmitData8(handle->bus->i2c, (*data));
-                    data++;
-                }
-                size--;
-            }
-
-            if(furi_hal_cortex_timer_is_expired(timer)) {
-                ret = false;
-                break;
-            }
-        }
-
-        LL_I2C_ClearFlag_STOP(handle->bus->i2c);
-    } while(0);
-
-    return ret;
+    return furi_hal_i2c_tx_ext(
+               handle,
+               i2c_addr,
+               false,
+               &mem_addr,
+               1,
+               FuriHalI2cBeginStart,
+               FuriHalI2cEndPause,
+               timeout) &&
+           furi_hal_i2c_tx_ext(
+               handle,
+               i2c_addr,
+               false,
+               data,
+               len,
+               FuriHalI2cBeginResume,
+               FuriHalI2cEndStop,
+               timeout);
 }
--- firmware/targets/furi_hal_include/furi_hal_i2c.h
+++ firmware/targets/furi_hal_include/furi_hal_i2c.h
@@ -1,17 +1,46 @@
 /**
- * @file furi_hal_i2c.h
- * I2C HAL API
+ * @file furi_hal_i2c.h I2C HAL API
  */
 
 #pragma once
 
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <furi_hal_i2c_config.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/** Transaction beginning signal */
+typedef enum {
+    /*!Begin the transaction by sending a START condition followed by the
+     * address */
+    FuriHalI2cBeginStart,
+    /*!Begin the transaction by sending a RESTART condition followed by the
+     * address
+     * @note       Must follow a transaction ended with
+     *             FuriHalI2cEndAwaitRestart */
+    FuriHalI2cBeginRestart,
+    /*!Continue the previous transaction with new data
+     * @note       Must follow a transaction ended with FuriHalI2cEndPause and
+     *             be of the same type (RX/TX) */
+    FuriHalI2cBeginResume,
+} FuriHalI2cBegin;
+
+/** Transaction end signal */
+typedef enum {
+    /*!End the transaction by sending a STOP condition */
+    FuriHalI2cEndStop,
+    /*!End the transaction by clock stretching
+     * @note       Must be followed by a transaction using
+     *             FuriHalI2cBeginRestart */
+    FuriHalI2cEndAwaitRestart,
+    /*!Pauses the transaction by clock stretching
+     * @note       Must be followed by a transaction using FuriHalI2cBeginResume */
+    FuriHalI2cEndPause,
+} FuriHalI2cEnd;
 
 /** Early Init I2C */
 void furi_hal_i2c_init_early();
@@ -22,78 +51,126 @@ void furi_hal_i2c_deinit_early();
 /** Init I2C */
 void furi_hal_i2c_init();
 
-/** Acquire i2c bus handle
+/** Acquire I2C bus handle
  *
- * @return     Instance of FuriHalI2cBus
+ * @param      handle  Pointer to FuriHalI2cBusHandle instance
  */
 void furi_hal_i2c_acquire(FuriHalI2cBusHandle* handle);
 
-/** Release i2c bus handle
- *
- * @param      bus   instance of FuriHalI2cBus aquired in `furi_hal_i2c_acquire`
+/** Release I2C bus handle
+ * 
+ * @param      handle  Pointer to FuriHalI2cBusHandle instance acquired in
+ *                     `furi_hal_i2c_acquire`
  */
 void furi_hal_i2c_release(FuriHalI2cBusHandle* handle);
 
-/** Perform I2C tx transfer
+/** Perform I2C TX transfer
  *
- * @param      handle   pointer to FuriHalI2cBusHandle instance
+ * @param      handle   Pointer to FuriHalI2cBusHandle instance
  * @param      address  I2C slave address
- * @param      data     pointer to data buffer
- * @param      size     size of data buffer
- * @param      timeout  timeout in ticks
+ * @param      data     Pointer to data buffer
+ * @param      size     Size of data buffer
+ * @param      timeout  Timeout in milliseconds
  *
  * @return     true on successful transfer, false otherwise
  */
 bool furi_hal_i2c_tx(
     FuriHalI2cBusHandle* handle,
-    const uint8_t address,
+    uint8_t address,
     const uint8_t* data,
-    const uint8_t size,
+    size_t size,
     uint32_t timeout);
 
-/** Perform I2C rx transfer
+/**
+ * Perform I2C TX transfer, with additional settings.
  *
- * @param      handle   pointer to FuriHalI2cBusHandle instance
+ * @param      handle   Pointer to FuriHalI2cBusHandle instance
  * @param      address  I2C slave address
- * @param      data     pointer to data buffer
- * @param      size     size of data buffer
- * @param      timeout  timeout in ticks
+ * @param      ten_bit  Whether the address is 10 bits wide
+ * @param      data     Pointer to data buffer
+ * @param      size     Size of data buffer
+ * @param      begin    How to begin the transaction
+ * @param      end      How to end the transaction
+ * @param      timer    Timeout timer
+ *
+ * @return     true on successful transfer, false otherwise
+ */
+bool furi_hal_i2c_tx_ext(
+    FuriHalI2cBusHandle* handle,
+    uint16_t address,
+    bool ten_bit,
+    const uint8_t* data,
+    size_t size,
+    FuriHalI2cBegin begin,
+    FuriHalI2cEnd end,
+    uint32_t timeout);
+
+/** Perform I2C RX transfer
+ *
+ * @param      handle   Pointer to FuriHalI2cBusHandle instance
+ * @param      address  I2C slave address
+ * @param      data     Pointer to data buffer
+ * @param      size     Size of data buffer
+ * @param      timeout  Timeout in milliseconds
  *
  * @return     true on successful transfer, false otherwise
  */
 bool furi_hal_i2c_rx(
     FuriHalI2cBusHandle* handle,
-    const uint8_t address,
+    uint8_t address,
     uint8_t* data,
-    const uint8_t size,
+    size_t size,
     uint32_t timeout);
 
-/** Perform I2C tx and rx transfers
+/** Perform I2C RX transfer, with additional settings.
  *
- * @param      handle   pointer to FuriHalI2cBusHandle instance
+ * @param      handle   Pointer to FuriHalI2cBusHandle instance
  * @param      address  I2C slave address
- * @param      tx_data  pointer to tx data buffer
- * @param      tx_size  size of tx data buffer
- * @param      rx_data  pointer to rx data buffer
- * @param      rx_size  size of rx data buffer
- * @param      timeout  timeout in ticks
+ * @param      ten_bit  Whether the address is 10 bits wide
+ * @param      data     Pointer to data buffer
+ * @param      size     Size of data buffer
+ * @param      begin    How to begin the transaction
+ * @param      end      How to end the transaction
+ * @param      timer    Timeout timer
+ *
+ * @return     true on successful transfer, false otherwise
+ */
+bool furi_hal_i2c_rx_ext(
+    FuriHalI2cBusHandle* handle,
+    uint16_t address,
+    bool ten_bit,
+    uint8_t* data,
+    size_t size,
+    FuriHalI2cBegin begin,
+    FuriHalI2cEnd end,
+    uint32_t timeout);
+
+/** Perform I2C TX and RX transfers
+ *
+ * @param      handle   Pointer to FuriHalI2cBusHandle instance
+ * @param      address  I2C slave address
+ * @param      tx_data  Pointer to TX data buffer
+ * @param      tx_size  Size of TX data buffer
+ * @param      rx_data  Pointer to RX data buffer
+ * @param      rx_size  Size of RX data buffer
+ * @param      timeout  Timeout in milliseconds
  *
  * @return     true on successful transfer, false otherwise
  */
 bool furi_hal_i2c_trx(
     FuriHalI2cBusHandle* handle,
-    const uint8_t address,
+    uint8_t address,
     const uint8_t* tx_data,
-    const uint8_t tx_size,
+    size_t tx_size,
     uint8_t* rx_data,
-    const uint8_t rx_size,
+    size_t rx_size,
     uint32_t timeout);
 
 /** Check if I2C device presents on bus
  *
- * @param      handle   pointer to FuriHalI2cBusHandle instance
- * @param      i2c_addr I2C slave address
- * @param      timeout  timeout in ticks
+ * @param      handle    Pointer to FuriHalI2cBusHandle instance
+ * @param      i2c_addr  I2C slave address
+ * @param      timeout   Timeout in milliseconds
  *
  * @return     true if device present and is ready, false otherwise
  */
@@ -101,11 +178,11 @@ bool furi_hal_i2c_is_device_ready(FuriHalI2cBusHandle* handle, uint8_t i2c_addr,
 
 /** Perform I2C device register read (8-bit)
  *
- * @param      handle   pointer to FuriHalI2cBusHandle instance
- * @param      i2c_addr I2C slave address
- * @param      reg_addr register address
- * @param      data     pointer to register value
- * @param      timeout  timeout in ticks
+ * @param      handle    Pointer to FuriHalI2cBusHandle instance
+ * @param      i2c_addr  I2C slave address
+ * @param      reg_addr  Register address
+ * @param      data      Pointer to register value
+ * @param      timeout   Timeout in milliseconds
  *
  * @return     true on successful transfer, false otherwise
  */
@@ -118,11 +195,11 @@ bool furi_hal_i2c_read_reg_8(
 
 /** Perform I2C device register read (16-bit)
  *
- * @param      handle   pointer to FuriHalI2cBusHandle instance
- * @param      i2c_addr I2C slave address
- * @param      reg_addr register address
- * @param      data     pointer to register value
- * @param      timeout  timeout in ticks
+ * @param      handle    Pointer to FuriHalI2cBusHandle instance
+ * @param      i2c_addr  I2C slave address
+ * @param      reg_addr  Register address
+ * @param      data      Pointer to register value
+ * @param      timeout   Timeout in milliseconds
  *
  * @return     true on successful transfer, false otherwise
  */
@@ -135,12 +212,12 @@ bool furi_hal_i2c_read_reg_16(
 
 /** Perform I2C device memory read
  *
- * @param      handle   pointer to FuriHalI2cBusHandle instance
- * @param      i2c_addr I2C slave address
- * @param      mem_addr memory start address
- * @param      data     pointer to data buffer
- * @param      len      size of data buffer
- * @param      timeout  timeout in ticks
+ * @param      handle    Pointer to FuriHalI2cBusHandle instance
+ * @param      i2c_addr  I2C slave address
+ * @param      mem_addr  Memory start address
+ * @param      data      Pointer to data buffer
+ * @param      len       Size of data buffer
+ * @param      timeout   Timeout in milliseconds
  *
  * @return     true on successful transfer, false otherwise
  */
@@ -149,16 +226,16 @@ bool furi_hal_i2c_read_mem(
     uint8_t i2c_addr,
     uint8_t mem_addr,
     uint8_t* data,
-    uint8_t len,
+    size_t len,
     uint32_t timeout);
 
 /** Perform I2C device register write (8-bit)
  *
- * @param      handle   pointer to FuriHalI2cBusHandle instance
- * @param      i2c_addr I2C slave address
- * @param      reg_addr register address
- * @param      data     register value
- * @param      timeout  timeout in ticks
+ * @param      handle    Pointer to FuriHalI2cBusHandle instance
+ * @param      i2c_addr  I2C slave address
+ * @param      reg_addr  Register address
+ * @param      data      Register value
+ * @param      timeout   Timeout in milliseconds
  *
  * @return     true on successful transfer, false otherwise
  */
@@ -171,11 +248,11 @@ bool furi_hal_i2c_write_reg_8(
 
 /** Perform I2C device register write (16-bit)
  *
- * @param      handle   pointer to FuriHalI2cBusHandle instance
- * @param      i2c_addr I2C slave address
- * @param      reg_addr register address
- * @param      data     register value
- * @param      timeout  timeout in ticks
+ * @param      handle    Pointer to FuriHalI2cBusHandle instance
+ * @param      i2c_addr  I2C slave address
+ * @param      reg_addr  Register address
+ * @param      data      Register value
+ * @param      timeout   Timeout in milliseconds
  *
  * @return     true on successful transfer, false otherwise
  */
@@ -188,12 +265,12 @@ bool furi_hal_i2c_write_reg_16(
 
 /** Perform I2C device memory
  *
- * @param      handle   pointer to FuriHalI2cBusHandle instance
- * @param      i2c_addr I2C slave address
- * @param      mem_addr memory start address
- * @param      data     pointer to data buffer
- * @param      len      size of data buffer
- * @param      timeout  timeout in ticks
+ * @param      handle    Pointer to FuriHalI2cBusHandle instance
+ * @param      i2c_addr  I2C slave address
+ * @param      mem_addr  Memory start address
+ * @param      data      Pointer to data buffer
+ * @param      len       Size of data buffer
+ * @param      timeout   Timeout in milliseconds
  *
  * @return     true on successful transfer, false otherwise
  */
@@ -201,8 +278,8 @@ bool furi_hal_i2c_write_mem(
     FuriHalI2cBusHandle* handle,
     uint8_t i2c_addr,
     uint8_t mem_addr,
-    uint8_t* data,
-    uint8_t len,
+    const uint8_t* data,
+    size_t len,
     uint32_t timeout);
 
 #ifdef __cplusplus
