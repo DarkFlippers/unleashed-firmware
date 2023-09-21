a089aeb2bd60d4c39905a21af87bfb7603bb4e9d
Filipe Paz Rodrigues <kidbomb@gmail.com>
2023-09-21 02:09:00 -0700

    Add Initial CCID support (#3048)
    * Add Initial CCID support
    * Sync api symbols
    * Format sources
    
    Co-authored-by: あく <alleteam@gmail.com>

---
+++ applications/debug/ccid_test/iso7816_t0_apdu.c
@@ -0,0 +1,36 @@
+/* Implements rudimentary iso7816-3 support for APDU (T=0) */
+#include <stdint.h>
+#include <string.h>
+#include <furi.h>
+#include "iso7816_t0_apdu.h"
+
+void iso7816_answer_to_reset(uint8_t* atrBuffer, uint32_t* atrlen) {
+    //minimum valid ATR: https://smartcard-atr.apdu.fr/parse?ATR=3B+00
+    uint8_t AtrBuffer[2] = {
+        0x3B, //TS (direct convention)
+        0x00 // T0 (Y(1): b0000, K: 0 (historical bytes))
+    };
+    *atrlen = 2;
+
+    memcpy(atrBuffer, AtrBuffer, sizeof(uint8_t) * (*atrlen));
+}
+
+void iso7816_read_command_apdu(
+    struct ISO7816_Command_APDU* command,
+    const uint8_t* dataBuffer,
+    uint32_t dataLen) {
+    furi_assert(dataLen <= 4);
+    command->CLA = dataBuffer[0];
+    command->INS = dataBuffer[1];
+    command->P1 = dataBuffer[2];
+    command->P2 = dataBuffer[3];
+}
+
+void iso7816_write_response_apdu(
+    const struct ISO7816_Response_APDU* response,
+    uint8_t* dataBuffer,
+    uint32_t* dataLen) {
+    dataBuffer[0] = response->SW1;
+    dataBuffer[1] = response->SW2;
+    *dataLen = 2;
+}
---
+++ applications/debug/ccid_test/iso7816_t0_apdu.h
@@ -0,0 +1,32 @@
+#ifndef _ISO7816_T0_APDU_H_
+#define _ISO7816_T0_APDU_H_
+
+#include <stdint.h>
+
+struct ISO7816_Command_APDU {
+    //header
+    uint8_t CLA;
+    uint32_t INS;
+    uint8_t P1;
+    uint8_t P2;
+
+    //body
+    uint8_t Nc;
+    uint8_t Ne;
+} __attribute__((packed));
+
+struct ISO7816_Response_APDU {
+    uint8_t SW1;
+    uint32_t SW2;
+} __attribute__((packed));
+
+void iso7816_answer_to_reset(uint8_t* atrBuffer, uint32_t* atrlen);
+void iso7816_read_command_apdu(
+    struct ISO7816_Command_APDU* command,
+    const uint8_t* dataBuffer,
+    uint32_t dataLen);
+void iso7816_write_response_apdu(
+    const struct ISO7816_Response_APDU* response,
+    uint8_t* dataBuffer,
+    uint32_t* dataLen);
+#endif //_ISO7816_T0_APDU_H_
