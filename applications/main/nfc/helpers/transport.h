8188b63522f1a2b59c687f0d975ad0f17c554af9
assasinfil <nfa57643@gmail.com>
2023-11-20 18:43:23 +0300

    cleanup

--- applications/main/nfc/helpers/transport.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <furi_hal_rtc.h>
-#include <nfc/helpers/bit_lib.h>
-#include <nfc/protocols/mf_classic/mf_classic.h>
-
-bool parse_transport_block(const MfClassicBlock* block, FuriString* result);
