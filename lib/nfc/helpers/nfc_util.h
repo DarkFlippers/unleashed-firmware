36aecfbec98bdb6c403c6eeb7e6f7eba884bc5d1
Leptopt1los <53914086+Leptopt1los@users.noreply.github.com>
2023-12-16 02:05:16 +0900

    nfc_util: little endian bytes2num functions added (#3287)
    * nfc_util_bytes2num_little_endian function added
    * f18 target api version sync
    * Bump api version
    
    Co-authored-by: hedger <hedger@users.noreply.github.com>
    Co-authored-by: あく <alleteam@gmail.com>

--- lib/nfc/helpers/nfc_util.h
+++ lib/nfc/helpers/nfc_util.h
@@ -10,6 +10,8 @@ void nfc_util_num2bytes(uint64_t src, uint8_t len, uint8_t* dest);
 
 uint64_t nfc_util_bytes2num(const uint8_t* src, uint8_t len);
 
+uint64_t nfc_util_bytes2num_little_endian(const uint8_t* src, uint8_t len);
+
 uint8_t nfc_util_even_parity32(uint32_t data);
 
 uint8_t nfc_util_odd_parity8(uint8_t data);
