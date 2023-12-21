8fa21c49b2ee49aadd28d713f586880469e3bff1
MX <10697207+xMasterX@users.noreply.github.com>
2023-12-21 03:16:16 +0300

    better subghz history element removal
    by Willy-JL
    
    https://github.com/Flipper-XFW/Xtreme-Firmware/commit/c40755f700a89af2b6519e7cd9a5d64f11c95165

--- applications/main/subghz/views/receiver.c
+++ applications/main/subghz/views/receiver.c
@@ -655,39 +655,31 @@ uint16_t subghz_view_receiver_get_idx_menu(SubGhzViewReceiver* subghz_receiver) 
     return idx;
 }
 
-void subghz_view_receiver_delete_element_callback(SubGhzViewReceiver* subghz_receiver) {
+void subghz_view_receiver_delete_item(SubGhzViewReceiver* subghz_receiver, uint16_t idx) {
     furi_assert(subghz_receiver);
 
     with_view_model(
         subghz_receiver->view,
         SubGhzViewReceiverModel * model,
         {
-            SubGhzReceiverMenuItemArray_it_t it;
-            // SubGhzReceiverMenuItem* target_item =
-            //     SubGhzReceiverMenuItemArray_get(model->history->data, model->idx);
-            SubGhzReceiverMenuItemArray_it_last(it, model->history->data);
-            while(!SubGhzReceiverMenuItemArray_end_p(it)) {
-                SubGhzReceiverMenuItem* item = SubGhzReceiverMenuItemArray_ref(it);
+            if(idx < SubGhzReceiverMenuItemArray_size(model->history->data)) {
+                SubGhzReceiverMenuItem* item =
+                    SubGhzReceiverMenuItemArray_get(model->history->data, idx);
+                furi_string_free(item->item_str);
+                furi_string_free(item->time);
+                item->type = 0;
+                SubGhzReceiverMenuItemArray_remove_v(model->history->data, idx, idx + 1);
 
-                if(it->index == (size_t)(model->idx)) {
-                    furi_string_free(item->item_str);
-                    furi_string_free(item->time);
-                    item->type = 0;
-                    SubGhzReceiverMenuItemArray_remove(model->history->data, it);
+                if(model->history_item == 5) {
+                    if(model->idx >= 2) {
+                        model->idx = model->history_item - 1;
+                    }
                 }
+                model->history_item--;
 
-                SubGhzReceiverMenuItemArray_previous(it);
-            }
-
-            if(model->history_item == 5) {
-                if(model->idx >= 2) {
-                    model->idx = model->history_item - 1;
+                if(model->idx && (model->idx > idx || model->idx == model->history_item)) {
+                    model->idx--;
                 }
-            }
-            model->history_item--;
-
-            if(model->idx != 0) {
-                model->idx--;
             }
         },
         true);
