c8ce5e6ed46f90582cff4e7dbdd516c684fc7b49
MX <10697207+xMasterX@users.noreply.github.com>
2024-01-02 14:10:19 +0300

    replace icon

--- applications/main/lfrfid/scenes/lfrfid_scene_clear_t5577.c
+++ applications/main/lfrfid/scenes/lfrfid_scene_clear_t5577.c
@@ -55,8 +55,8 @@ void lfrfid_scene_clear_t5577_on_enter(void* context) {
     lfrfid_clear_t5577_password_and_config_to_EM(app);
 
     notification_message(app->notifications, &sequence_success);
-    popup_set_header(popup, "Done!", 94, 10, AlignCenter, AlignTop);
-    popup_set_icon(popup, 0, 7, &I_RFIDDolphinSuccess_108x57);
+    popup_set_header(popup, "Success!", 75, 10, AlignLeft, AlignTop);
+    popup_set_icon(popup, 0, 9, &I_DolphinSuccess_91x55);
     popup_set_context(popup, app);
     popup_set_callback(popup, lfrfid_popup_timeout_callback);
     popup_set_timeout(popup, 1500);
