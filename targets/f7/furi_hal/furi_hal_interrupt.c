dd182ab179abec3513d9ab97b51e0ff84bbd833a
あく <alleteam@gmail.com>
2024-01-16 08:17:07 +0900

    FuriHal: interrupt priorities and documentation (#3366)
    * FuriHal: interrupt priorities and documentation
    * FuriHal: wording
    * FuriHal: update interrupt docs
    * FuriHal: add more interrupt priority levels
    * FuriHal: proper furi_check in interrupts, shift default level to 10
    
    ---------
    
    Co-authored-by: hedger <hedger@users.noreply.github.com>

--- targets/f7/furi_hal/furi_hal_interrupt.c
+++ targets/f7/furi_hal/furi_hal_interrupt.c
@@ -11,7 +11,7 @@
 
 #define TAG "FuriHalInterrupt"
 
-#define FURI_HAL_INTERRUPT_DEFAULT_PRIORITY (configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY)
+#define FURI_HAL_INTERRUPT_DEFAULT_PRIORITY (configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY + 5)
 
 typedef struct {
     FuriHalInterruptISR isr;
@@ -126,16 +126,21 @@ void furi_hal_interrupt_init() {
 }
 
 void furi_hal_interrupt_set_isr(FuriHalInterruptId index, FuriHalInterruptISR isr, void* context) {
-    furi_hal_interrupt_set_isr_ex(index, FURI_HAL_INTERRUPT_DEFAULT_PRIORITY, isr, context);
+    furi_hal_interrupt_set_isr_ex(index, FuriHalInterruptPriorityNormal, isr, context);
 }
 
 void furi_hal_interrupt_set_isr_ex(
     FuriHalInterruptId index,
-    uint16_t priority,
+    FuriHalInterruptPriority priority,
     FuriHalInterruptISR isr,
     void* context) {
     furi_check(index < FuriHalInterruptIdMax);
-    furi_check(priority <= 15);
+    furi_check(
+        (priority >= FuriHalInterruptPriorityLowest &&
+         priority <= FuriHalInterruptPriorityHighest) ||
+        priority == FuriHalInterruptPriorityKamiSama);
+
+    uint16_t real_priority = FURI_HAL_INTERRUPT_DEFAULT_PRIORITY - priority;
 
     if(isr) {
         // Pre ISR set
@@ -153,7 +158,7 @@ void furi_hal_interrupt_set_isr_ex(
     if(isr) {
         // Post ISR set
         furi_hal_interrupt_clear_pending(index);
-        furi_hal_interrupt_enable(index, priority);
+        furi_hal_interrupt_enable(index, real_priority);
     } else {
         // Post ISR clear
     }
