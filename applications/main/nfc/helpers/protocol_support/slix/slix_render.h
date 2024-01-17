5655be1b8fbbb8e6444c05523744df8ef244dbe0
RebornedBrain <RebornedBrain@gmail.com>
2024-01-17 20:07:28 +0300

    New info render format for slix cards.
    Also More_Info scene added to display Memory data

--- applications/main/nfc/helpers/protocol_support/slix/slix_render.h
+++ applications/main/nfc/helpers/protocol_support/slix/slix_render.h
@@ -3,5 +3,6 @@
 #include <nfc/protocols/slix/slix.h>
 
 #include "../nfc_protocol_support_render_common.h"
+#include "../iso15693_3/iso15693_3_render.h"
 
 void nfc_render_slix_info(const SlixData* data, NfcProtocolFormatType format_type, FuriString* str);
