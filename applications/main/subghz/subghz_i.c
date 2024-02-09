c0db3d541e6fe6ea202b909044c9fd3f5056831d
Skorpionm <85568270+Skorpionm@users.noreply.github.com>
2024-02-09 12:49:58 +0400

    [FL-3753] UI SubGhz: fix UI only_rx scene (#3379)
    * UI SubGhz: fix UI only_rx scene
    * UI SubGhz: delete unused scene
    
    Co-authored-by: あく <alleteam@gmail.com>

--- applications/main/subghz/subghz_i.c
+++ applications/main/subghz/subghz_i.c
@@ -60,15 +60,15 @@ void subghz_dialog_message_show_only_rx(SubGhz* subghz) {
     DialogsApp* dialogs = subghz->dialogs;
     DialogMessage* message = dialog_message_alloc();
 
-    const char* header_text = "Transmission is blocked";
-    const char* message_text = "Transmission on\nthis frequency is\nrestricted in\nyour region";
+    const char* header_text = "Transmission is Blocked!";
+    const char* message_text = "Transmission on\nthis frequency is\nrestricted in your\nregion";
     if(!furi_hal_region_is_provisioned()) {
         header_text = "Firmware update needed";
         message_text = "Please update\nfirmware before\nusing this feature\nflipp.dev/upd";
     }
 
-    dialog_message_set_header(message, header_text, 63, 3, AlignCenter, AlignTop);
-    dialog_message_set_text(message, message_text, 0, 17, AlignLeft, AlignTop);
+    dialog_message_set_header(message, header_text, 63, 0, AlignCenter, AlignTop);
+    dialog_message_set_text(message, message_text, 1, 13, AlignLeft, AlignTop);
 
     dialog_message_set_icon(message, &I_WarningDolphinFlip_45x42, 83, 22);
 
