8ec1c74baf51809f00f463c2df0564b656d34207
Silent <CookiePLMonster@users.noreply.github.com>
2024-03-25 19:48:18 +0100

    SavedStruct: Introduce saved_struct_get_metadata (#3392)
    * SavedStruct: Replace saved_struct_get_payload_size with saved_struct_get_metadata
      This new function can obtain the magic value, version and payload size
      all at once. This makes the function useful e.g.
      for backwards compatibility.
    * SavedStruct: const-qualify saved_struct_save
    * Toolbox: add saved struct documentation
    
    Co-authored-by: あく <alleteam@gmail.com>

--- applications/services/expansion/expansion_settings.c
+++ applications/services/expansion/expansion_settings.c
@@ -19,7 +19,7 @@ bool expansion_settings_load(ExpansionSettings* settings) {
         EXPANSION_SETTINGS_VERSION);
 }
 
-bool expansion_settings_save(ExpansionSettings* settings) {
+bool expansion_settings_save(const ExpansionSettings* settings) {
     furi_assert(settings);
     return saved_struct_save(
         EXPANSION_SETTINGS_PATH,
