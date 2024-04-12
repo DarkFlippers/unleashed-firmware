f4ffd29bbc2282f86043e840127c17e3a3e618b9
MX <10697207+xMasterX@users.noreply.github.com>
2024-04-12 20:16:42 +0300

    Allow no prefix usage of name_generator_make_detailed_datetime
    Remove extra check for NULL since check is present in code already

--- lib/toolbox/name_generator.c
+++ lib/toolbox/name_generator.c
@@ -74,7 +74,6 @@ void name_generator_make_detailed_datetime(
     DateTime* custom_time) {
     furi_check(name);
     furi_check(max_name_size);
-    furi_check(prefix);
 
     DateTime dateTime;
     if(custom_time) {
