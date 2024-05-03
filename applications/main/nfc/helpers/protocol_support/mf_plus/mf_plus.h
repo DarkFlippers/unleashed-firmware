6bfa383cea9aa029b902d5c473743a171629c1d7
MX <10697207+xMasterX@users.noreply.github.com>
2024-05-03 14:50:58 +0300

    Revert "Merge remote-tracking branch 'OFW/astra/3746-mfp-detect' into dev"
    This reverts commit 9b377e6f80c8b201402adbad500edf1705926d9d, reversing
    changes made to 1736a8a7c2127ae246fac82a549c502ce0c3e79a.

--- applications/main/nfc/helpers/protocol_support/mf_plus/mf_plus.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "../nfc_protocol_support_base.h"
-
-extern const NfcProtocolSupportBase nfc_protocol_support_mf_plus;
--- lib/nfc/protocols/mf_plus/mf_plus_poller_defs.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <nfc/protocols/nfc_poller_base.h>
-
-extern const NfcPollerBase mf_plus_poller;
