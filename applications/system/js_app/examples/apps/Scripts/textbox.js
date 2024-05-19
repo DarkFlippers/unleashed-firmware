21abcb56fdb96a4fea568a0e326d7641787cd5b6
MX <10697207+xMasterX@users.noreply.github.com>
2024-05-19 03:54:21 +0300

    merge ofw dev missing parts

--- applications/system/js_app/examples/apps/Scripts/textbox.js
+++ applications/system/js_app/examples/apps/Scripts/textbox.js
@@ -4,9 +4,9 @@ let textbox = require("textbox");
 // Focus (start / end), Font (text / hex)
 textbox.setConfig("end", "text");
 
-// Can make sure it's empty before showing, in case of reusing in same script
-// (Closing textbox already empties the text, but maybe you added more in a loop for example)
-textbox.emptyText();
+// Can make sure it's cleared before showing, in case of reusing in same script
+// (Closing textbox already clears the text, but maybe you added more in a loop for example)
+textbox.clearText();
 
 // Add default text
 textbox.addText("Example dynamic updating textbox\n");
