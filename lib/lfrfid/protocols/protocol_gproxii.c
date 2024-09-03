a8482984de96c821350b82cb5711032ebba10d1b
Astra <me@astrra.space>
2024-09-03 17:42:49 +0900

    Fix incorrect parity starting bit

--- lib/lfrfid/protocols/protocol_gproxii.c
+++ lib/lfrfid/protocols/protocol_gproxii.c
@@ -89,8 +89,9 @@ static bool protocol_gproxii_can_be_decoded(ProtocolGProxII* protocol) {
     if(bit_lib_get_bits(protocol->data, 0, 6) != 0b111110) return false;
 
     // Check always 0 parity on every 5th bit after preamble
-    if(!bit_lib_test_parity(protocol->data, 5, GPROXII_ENCODED_BIT_SIZE, BitLibParityAlways0, 5)) {
+    if(!bit_lib_test_parity(protocol->data, 6, GPROXII_ENCODED_BIT_SIZE, BitLibParityAlways0, 5)) {
         return false;
+    }
 
     // Start GProx II decode
     bit_lib_copy_bits(protocol->decoded_data, 0, GPROXII_ENCODED_BIT_SIZE, protocol->data, 6);
