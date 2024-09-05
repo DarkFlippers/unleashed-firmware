b040db07f46ad148eac8f9a5eaf30215b16c6e12
DerSkythe <31771569+derskythe@users.noreply.github.com>
2024-09-05 17:50:33 +0400

    Gui: Add up and down button drawing functions to GUI elements (#3804)
    * feat: Add up and down button drawing functions to GUI elements
      Two button drawing functions, elements_button_up and elements_button_down, have been added to the GUI elements. These functions allow a button to be drawn at the top left and top right corner of the canvas respectively, with an accompanying string and icon. The underlying layout and design of these buttons is defined within these functions.
    * feat: Add null checks for Canvas parameter in button functions
      Added furi_check to ensure the Canvas parameter is not null in elements_button_up and elements_button_down functions. This prevents potential crashes due to dereferencing a null pointer.
    
    Co-authored-by: hedger <hedger@users.noreply.github.com>
    Co-authored-by: Aleksandr Kutuzov <alleteam@gmail.com>

--- applications/services/gui/elements.h
+++ applications/services/gui/elements.h
@@ -96,6 +96,28 @@ void elements_button_left(Canvas* canvas, const char* str);
  */
 void elements_button_right(Canvas* canvas, const char* str);
 
+/**
+ * @brief This function draws a button in the top left corner of the canvas with icon and string.
+ *
+ * The design and layout of the button is defined within this function.
+ *
+ * @param[in] canvas This is a pointer to the @c Canvas structure where the button will be drawn.
+ * @param[in] str This is a pointer to the character string that will be drawn within the button.
+ *
+ */
+void elements_button_up(Canvas* canvas, const char* str);
+
+/**
+ * @brief This function draws a button in the top right corner of the canvas with icon and string.
+ *
+ * The design and layout of the button is defined within this function.
+ *
+ * @param[in] canvas This is a pointer to the @c Canvas structure where the button will be drawn.
+ * @param[in] str This is a pointer to the character string that will be drawn within the button.
+ *
+ */
+void elements_button_down(Canvas* canvas, const char* str);
+
 /** Draw button in center
  *
  * @param   canvas  Canvas instance
