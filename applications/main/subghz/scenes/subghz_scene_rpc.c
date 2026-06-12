30d939d52fb02ebbbf1aabe743dfb573f45fc6ee
MX <10697207+xMasterX@users.noreply.github.com>
2026-06-12 16:58:39 +0300

    Fix endless TX causing RAW files to be transmitted and crash the system
    Fixes issue #1008

--- applications/main/subghz/scenes/subghz_scene_rpc.c
+++ applications/main/subghz/scenes/subghz_scene_rpc.c
@@ -51,7 +51,10 @@ bool subghz_scene_rpc_on_event(void* context, SceneManagerEvent event) {
         } else if(event.event == SubGhzCustomEventSceneRpcSessionClose) {
             scene_manager_stop(subghz->scene_manager);
             view_dispatcher_stop(subghz->view_dispatcher);
-        } else if(event.event == SubGhzCustomEventSceneRpcButtonPress) {
+        } else if(
+            event.event == SubGhzCustomEventSceneRpcButtonPress ||
+            event.event == SubGhzCustomEventSceneRpcButtonPressRelease) {
+            // RAW TX from the APP when button is held for full TX = SubGhzCustomEventSceneRpcButtonPressRelease
             bool result = false;
             if(state == SubGhzRpcStateLoaded) {
                 // START endless TX until user release button
@@ -92,48 +95,10 @@ bool subghz_scene_rpc_on_event(void* context, SceneManagerEvent event) {
                 subghz_block_generic_global.endless_tx = false;
                 result = true;
             }
+            // This will be called in EventTypeTick after TX is really stopped
             // scene_manager_set_scene_state(
             //     subghz->scene_manager, SubGhzSceneRpc, SubGhzRpcStateIdle);
             rpc_system_app_confirm(subghz->rpc_ctx, result);
-
-            // USELESS PART
-            // } else if(event.event == SubGhzCustomEventSceneRpcButtonPressRelease) {
-            //     bool result = false;
-            //     if(state == SubGhzRpcStateLoaded) {
-            //         switch(
-            //             subghz_txrx_tx_start(subghz->txrx, subghz_txrx_get_fff_data(subghz->txrx))) {
-            //         case SubGhzTxRxStartTxStateErrorOnlyRx:
-            //             rpc_system_app_set_error_code(
-            //                 subghz->rpc_ctx, RpcAppSystemErrorCodeRegionLock);
-            //             rpc_system_app_set_error_text(
-            //                 subghz->rpc_ctx,
-            //                 "Transmission on this frequency is restricted in your region");
-            //             break;
-            //         case SubGhzTxRxStartTxStateErrorParserOthers:
-            //             rpc_system_app_set_error_code(
-            //                 subghz->rpc_ctx, RpcAppSystemErrorCodeInternalParse);
-            //             rpc_system_app_set_error_text(
-            //                 subghz->rpc_ctx, "Error in protocol parameters description");
-            //             break;
-
-            //         default: //if(SubGhzTxRxStartTxStateOk)
-            //             result = true;
-            //             subghz_blink_start(subghz);
-            //             scene_manager_set_scene_state(
-            //                 subghz->scene_manager, SubGhzSceneRpc, SubGhzRpcStateTx);
-            //             break;
-            //         }
-            //     }
-
-            //     // Stop transmission
-            //     if(state == SubGhzRpcStateTx) {
-            //         subghz_txrx_stop(subghz->txrx);
-            //         subghz_blink_stop(subghz);
-            //         result = true;
-            //     }
-            //     scene_manager_set_scene_state(
-            //         subghz->scene_manager, SubGhzSceneRpc, SubGhzRpcStateIdle);
-            //     rpc_system_app_confirm(subghz->rpc_ctx, result);
         } else if(event.event == SubGhzCustomEventSceneRpcLoad) {
             bool result = false;
             if(state == SubGhzRpcStateIdle) {
@@ -151,16 +116,32 @@ bool subghz_scene_rpc_on_event(void* context, SceneManagerEvent event) {
         }
     } else if(event.type == SceneManagerEventTypeTick) {
         // if hardware TX finished then stop TX correctly
-        if(subghz_devices_is_async_complete_tx(subghz->txrx->radio_device)) {
-            bool result = false;
+        if(subghz_devices_is_async_complete_tx(subghz->txrx->radio_device) &&
+           !subghz_block_generic_global.endless_tx) {
+            //bool result = false;
             if(state == SubGhzRpcStateTx) {
                 subghz_txrx_stop(subghz->txrx);
                 subghz_blink_stop(subghz);
-                result = true;
+                //result = true;
             }
             scene_manager_set_scene_state(
                 subghz->scene_manager, SubGhzSceneRpc, SubGhzRpcStateIdle);
-            rpc_system_app_confirm(subghz->rpc_ctx, result);
+            // We call confirm only on button events, not every time we stop TX since there will be no event code?!?!? Why this was here...
+            //rpc_system_app_confirm(subghz->rpc_ctx, result);
+        } else {
+            // For RAW files, restart TX while endless TX is enabled.
+            if((state == SubGhzRpcStateTx) && subghz_block_generic_global.endless_tx &&
+               (subghz_get_load_type_file(subghz) == SubGhzLoadTypeFileRaw) &&
+               subghz_devices_is_async_complete_tx(subghz->txrx->radio_device)) {
+                if(subghz_txrx_tx_start(subghz->txrx, subghz_txrx_get_fff_data(subghz->txrx)) !=
+                   SubGhzTxRxStartTxStateOk) {
+                    subghz_block_generic_global.endless_tx = false;
+                    subghz_txrx_stop(subghz->txrx);
+                    subghz_blink_stop(subghz);
+                    scene_manager_set_scene_state(
+                        subghz->scene_manager, SubGhzSceneRpc, SubGhzRpcStateIdle);
+                }
+            }
         }
     }
     return consumed;
