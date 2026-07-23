2fe5605034554cb45d63da5e4d544374e1f5a845
SegerEnd <76704606+SegerEnd@users.noreply.github.com>
2026-07-23 18:30:41 +0200

    NFC: fix iso15693 emulation dropping Read Multiple Blocks for tags >64 blocks (#4420)

--- lib/signal_reader/parsers/iso15693/iso15693_parser.c
+++ lib/signal_reader/parsers/iso15693/iso15693_parser.c
@@ -192,6 +192,10 @@ static Iso15693ParserCommand iso15693_parser_parse_1_out_of_4(Iso15693Parser* in
                 instance->next_byte_part++;
                 if(instance->next_byte_part == 4) {
                     instance->next_byte_part = 0;
+                    // Overlong frame (noise): drop it instead of overflowing parsed_frame
+                    if(bit_buffer_get_size_bytes(instance->parsed_frame) >=
+                       bit_buffer_get_capacity_bytes(instance->parsed_frame))
+                        return Iso15693ParserCommandFail;
                     bit_buffer_append_byte(instance->parsed_frame, instance->next_byte);
                     instance->next_byte = 0;
                 }
@@ -239,6 +243,10 @@ static Iso15693ParserCommand iso15693_parser_parse_1_out_of_256(Iso15693Parser* 
             if(instance->bitstream_buff[i] != 0x00) {
                 for(size_t j = 0; j < 8; j++) {
                     if(FURI_BIT(instance->bitstream_buff[i], j) == 1) {
+                        // Overlong frame (noise): drop it instead of overflowing parsed_frame
+                        if(bit_buffer_get_size_bytes(instance->parsed_frame) >=
+                           bit_buffer_get_capacity_bytes(instance->parsed_frame))
+                            return Iso15693ParserCommandFail;
                         bit_buffer_append_byte(
                             instance->parsed_frame, instance->next_byte_part * 4 + j / 2);
                     }
