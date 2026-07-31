15e208c0ca29f08ea90a5bc30dc3c2c5adc186d9
Endika Iglesias <endika2@gmail.com>
2026-07-31 12:43:59 +0200

    [NFC] Bound card-controlled TLV lengths in the EMV poller (#1048)
    * [NFC] Bound card-controlled TLV lengths in the EMV poller
    
    * [NFC] Bound the EMV parser where the values actually live
    
    * [NFC] Bound the PDOL against what the poller can transmit
    
    * [NFC] Keep cardholder names a card may legally send
    
    * [NFC] Make rejected tags and truncated log records visible

--- lib/nfc/protocols/emv/emv.h
+++ lib/nfc/protocols/emv/emv.h
@@ -54,6 +54,8 @@ extern "C" {
 
 typedef struct {
     uint16_t tag;
+    // The card asks for a length of its own, so the terminal has to know how much it holds
+    uint8_t size;
     uint8_t data[];
 } PDOLValue;
 
@@ -85,7 +87,8 @@ typedef struct {
     uint8_t application_interchange_profile[2];
     char application_name[16 + 1];
     char application_label[16 + 1];
-    char cardholder_name[24 + 1];
+    // EMV Book 3 Annex A gives 5F20 an ans length of 2-26
+    char cardholder_name[26 + 1];
     uint8_t pan[10]; // card_number
     uint8_t pan_len;
     uint8_t exp_day;
